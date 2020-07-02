1c351a957336625d5ac4b57d37bf13f8acc202d2
N.O.R.T.H <yormen1@gmail.com>
2020-07-02 11:46:02 +0000

    Added a boolean condition to display button when sample is checked as received

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/AdditionalTestListComponent.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/AdditionalTestListComponent.java
@@ -37,7 +37,6 @@ public class AdditionalTestListComponent extends VerticalLayout {
 		testsHeader.addStyleName(CssStyles.H3);
 		componentHeader.addComponent(testsHeader);
 
-		if (UserProvider.getCurrent().hasUserRight(UserRight.ADDITIONAL_TEST_CREATE)) {
 			createButton = ButtonHelper.createIconButton(Captions.additionalTestNewTest, VaadinIcons.PLUS_CIRCLE, e -> {
 				ControllerProvider.getAdditionalTestController().openCreateComponent(sampleUuid, list::reload);
 			}, ValoTheme.BUTTON_PRIMARY);
