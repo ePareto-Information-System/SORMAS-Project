174a9c8d02d98d856e009545810d9d6f7511b101
N.O.R.T.H <yormen1@gmail.com>
2020-07-29 16:18:38 +0000

    Added a function to be call if the fields sample ID exists on the uuid parsed on not

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleController.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleController.java
@@ -190,11 +190,11 @@ public class SampleController {
 			form.getFieldGroup());
 		editView.addCommitListener(() -> {
 			if (!form.getFieldGroup().isModified()) {
+
 				SampleDto changedDto = form.getValue();
 				SampleDto originalDto = FacadeProvider.getSampleFacade().getSampleByUuid(changedDto.getUuid());
 				FacadeProvider.getSampleFacade().saveSample(changedDto);
 				SormasUI.refreshView();
-
 				if (changedDto.getSpecimenCondition() != originalDto.getSpecimenCondition()
 					&& changedDto.getSpecimenCondition() == SpecimenCondition.NOT_ADEQUATE
 					&& UserProvider.getCurrent().hasUserRight(UserRight.TASK_CREATE)) {
@@ -413,5 +413,9 @@ public class SampleController {
 			field = false;
 		}
 		return field;
+	}
+
+	public boolean isFieldSampleIdExist(String uuid, String fieldSampleId) {
+		return FacadeProvider.getSampleFacade().getSampleByFieldSampleID(uuid, fieldSampleId);
 	}
 }
