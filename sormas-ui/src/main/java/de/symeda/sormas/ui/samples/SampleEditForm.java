efa300e42911adce2aa3332b72e75aea1fc1a40b
N.O.R.T.H <yormen1@gmail.com>
2020-07-29 16:22:23 +0000

    Created an inner class impementation from Validator to validate the field sample Id base on the uuid

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleEditForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleEditForm.java
@@ -20,6 +20,7 @@ package de.symeda.sormas.ui.samples;
 import static de.symeda.sormas.ui.utils.LayoutUtil.h3;
 import static de.symeda.sormas.ui.utils.LayoutUtil.loc;
 
+import com.vaadin.v7.data.Validator;
 import com.vaadin.v7.ui.ComboBox;
 
 import de.symeda.sormas.api.FacadeProvider;
@@ -27,6 +28,7 @@ import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.i18n.Strings;
 import de.symeda.sormas.api.sample.PathogenTestResultType;
 import de.symeda.sormas.api.sample.SampleDto;
+import de.symeda.sormas.ui.ControllerProvider;
 
 public class SampleEditForm extends AbstractSampleForm {
 
@@ -50,15 +52,17 @@ public class SampleEditForm extends AbstractSampleForm {
 
 		setVisibilities();
 
-
 		addValueChangeListener(e -> {
 			defaultValueChangeListener();
+
 			if (FacadeProvider.getPathogenTestFacade().hasPathogenTest(getValue().toReference())) {
 				getField(SampleDto.PATHOGEN_TEST_RESULT).setRequired(true);
 			} else {
 				getField(SampleDto.PATHOGEN_TEST_RESULT).setEnabled(false);
 			}
 		});
+
+		addValidators(SampleDto.FIELD_SAMPLE_ID, new FieldSampleIdEditValidator());
 	}
 
 	public void makePathogenTestResultRequired() {
@@ -74,5 +78,20 @@ public class SampleEditForm extends AbstractSampleForm {
 	@Override
 	protected String createHtmlLayout() {
 		return HTML_LAYOUT;
+	}
+
+	class FieldSampleIdEditValidator implements Validator {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void validate(Object value) throws InvalidValueException {
+			SampleDto dto = getValue();
+			if (value == null || value.equals(""))
+				return;
+
+			else if (!(value instanceof String && ControllerProvider.getSampleController().isFieldSampleIdExist(dto.getUuid(), (String) value)))
+				throw new InvalidValueException(I18nProperties.getString(Strings.messageFieldSampleIdExist));
+		}
 	}
 }
