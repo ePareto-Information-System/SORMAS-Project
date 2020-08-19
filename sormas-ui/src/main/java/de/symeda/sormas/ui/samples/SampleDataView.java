e34c13a4367938fe56d0545b4b8c3dc7b091ebf9
N.O.R.T.H <yormen1@gmail.com>
2020-08-19 16:37:02 +0000

    Removed unwanted condition

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleDataView.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleDataView.java
@@ -169,8 +169,6 @@ public class SampleDataView extends AbstractSampleView {
 
 			if (!editComponent.getWrappedComponent().getFieldGroup().isValid()) {
 				return Strings.messageFormHasErrorsPathogenTest;
-			} else if (sampleDto.getSamplePurpose() == SamplePurpose.EXTERNAL && (sampleDto.isReceived() && sampleDto.getReceivedDate() == null)
-				|| (!sampleDto.isReceived())) {
 				return Strings.messageCheckReceivedAndSelectedSampleDate;
 			}
 				return Strings.messageCheckReceivedAndSelectedSampleDate;
