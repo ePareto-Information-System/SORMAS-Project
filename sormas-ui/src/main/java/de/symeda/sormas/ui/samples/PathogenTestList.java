09a0b9c7223c20b14990e9289a7756a116637617
N.O.R.T.H <yormen1@gmail.com>
2020-08-24 17:48:10 +0000

    Removed the if statement and notification

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/PathogenTestList.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/PathogenTestList.java
@@ -77,7 +77,6 @@ public class PathogenTestList extends PaginationList<PathogenTestDto> {
 						ControllerProvider.getPathogenTestController()
 							.edit(pathogenTest, caseSampleCount, PathogenTestList.this::reload, onSavedPathogenTest);
 					} else {
-						Notification.show(null, I18nProperties.getString(createOrEditAllowedCallback.get()), Type.ERROR_MESSAGE);
 					}
 				});
 			}
