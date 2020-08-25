b2c0284799221427814eb59c17d77444fa918e82
N.O.R.T.H <yormen1@gmail.com>
2020-08-25 17:44:22 +0000

    Modified the string name

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
@@ -636,7 +636,7 @@ public interface Strings {
 	String messageUserRoleCombination = "messageUserRoleCombination";
 	String messageVisitsDeleted = "messageVisitsDeleted";
 	String messageWrongFileType = "messageWrongFileType";
-	String messageCheckReceivedAndSelectedSampleDate = "messageCheckReceivedAndSelectedSampleDate";
+	String messageReceiveSampleBeforeTestResult = "messageCheckReceivedAndSelectedSampleDate";
 
 	String min = "min";
 	String mmhg = "mmhg";
