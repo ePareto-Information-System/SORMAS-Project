ba8b350d163e98da299dbd56305b32972de7ce0e
N.O.R.T.H <yormen1@gmail.com>
2020-10-12 11:26:24 +0000

    Creating a view for the disease details

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/diseasedetails/DiseaseDetailsView.java
@@ -0,0 +1,23 @@
+package de.symeda.sormas.ui.dashboard.diseasedetails;
+
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+import de.symeda.sormas.ui.utils.AbstractView;
+
+@SuppressWarnings("serial")
+public class DiseaseDetailsView extends AbstractView {
+
+	public static final String ROOT_VIEW_NAME = "dashboard";
+
+	public DiseaseDetailsView() {
+		super(ROOT_VIEW_NAME);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
