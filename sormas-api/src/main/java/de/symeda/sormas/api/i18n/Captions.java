288b5854f8671f6b631d1c2240d4e871a6ade553
North <yormen1@gmail.com>
2021-01-05 14:08:25 +0000

    renmamed otheroutcomedetails to specifyOther Outcome
    # Conflicts:
    #	sormas-api/src/main/resources/captions.properties

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
@@ -234,7 +234,7 @@ public interface Captions {
 	String CaseData_notifyingClinicDetails = "CaseData.notifyingClinicDetails";
 	String CaseData_numberOfVisits = "CaseData.numberOfVisits";
 	String CaseData_outcome = "CaseData.outcome";
-	String CaseData_specify_other_outcome = "CaseData.otheroutcomedetails";
+	String CaseData_specify_other_outcome = "CaseData.specifyOtherOutcome";
 	String CaseData_outcomeDate = "CaseData.outcomeDate";
 	String CaseData_overwriteFollowUpUntil = "CaseData.overwriteFollowUpUntil";
 	String CaseData_person = "CaseData.person";
