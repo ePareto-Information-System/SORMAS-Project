7c19f47eb93fc5e750cb68dd9977cdd7610ec771
Fath <faatihi@yahoo.com>
2021-03-09 13:46:11 +0000

    ui: add ability to log user's viewing activities

--- sormas-ui/src/main/java/de/symeda/sormas/ui/UiUtil.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/UiUtil.java
@@ -1,6 +1,11 @@
 package de.symeda.sormas.ui;
 
+import com.vaadin.ui.JavaScript;
+
 import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.HasUuid;
+import de.symeda.sormas.api.auditlog.AuditLogEntryDto;
+import de.symeda.sormas.api.auditlog.ChangeType;
 import de.symeda.sormas.api.feature.FeatureType;
 import de.symeda.sormas.api.user.UserRight;
 
@@ -16,5 +21,21 @@ public class UiUtil {
 
 	public static boolean permitted(UserRight userRight) {
 		return permitted(null, userRight);
+	}
+	
+	public static void logActivity (HasUuid entity) {
+		logActivity(ChangeType.VIEW, entity);
+	}
+	
+	public static void logActivity (ChangeType activityType, HasUuid entity) {
+		JavaScript.getCurrent().execute(
+			"setTimeout(function () {" +
+				"de.symeda.sormas.ui.auditlog.logActivity({" +
+					"activityType: '" + activityType.name() + "'," +
+					"clazz: '" + AuditLogEntryDto.getEntityClazz(entity.getClass()) + "'," +
+					"uuid: '" + entity.getUuid() + "'," +
+				"});" +
+			"}, 2000)"
+		);
 	}
 }
