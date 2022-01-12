e9d6a2361b4d52d098a2cecccf9ee8469ec9244c
Anthony4m <amoahanthony41@gmail.com>
2022-01-11 16:13:21 -0800

    Added string definitions of finals created in caseExportDto

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
@@ -441,6 +441,13 @@ public interface Captions {
 	String CaseExport_quarantineInformation = "CaseExport.quarantineInformation";
 	String CaseExport_reportingUserName = "CaseExport.reportingUserName";
 	String CaseExport_reportingUserRoles = "CaseExport.reportingUserRoles";
+	String CaseExport_latitude = "CaseExport.latitude";
+	String CaseExport_longitude = "CaseExport.longitude";
+	String CaseExport_caseLatLonAccuracy = "CaseExport.caseLatLonAccuracy";
+	String CaseExport_personLatitude = "CaseExport.personLatitude";
+	String CaseExport_personLatLongitude = "CaseExport.personLongitude";
+	String CaseExport_caseGpsCoordinates = "CaseExport.caseGpsCoordinates";
+	String CaseExport_personLatLonAccuracy = "CaseExport.personLatLonAccuracy";
 	String CaseExport_sampleDates = "CaseExport.sampleDates";
 	String CaseExport_sampleDateTime1 = "CaseExport.sampleDateTime1";
 	String CaseExport_sampleDateTime2 = "CaseExport.sampleDateTime2";
