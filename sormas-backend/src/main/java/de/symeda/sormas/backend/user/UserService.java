2b847bf1fac9bb924a114c054ce62edd4bcdbf2b
Anthony4m <amoahanthony41@gmail.com>
2022-02-21 23:12:11 -0800

    #21-Added generated password and update Password to user service

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
@@ -1,20 +1,17 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
  * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 package de.symeda.sormas.backend.user;
 
 import java.util.Arrays;
@@ -59,9 +56,9 @@ import de.symeda.sormas.backend.common.AdoServiceWithUserFilter;
 import de.symeda.sormas.backend.common.ConfigFacadeEjb.ConfigFacadeEjbLocal;
 import de.symeda.sormas.backend.common.CriteriaBuilderHelper;
 import de.symeda.sormas.backend.event.Event;
-import de.symeda.sormas.backend.infrastructure.facility.Facility;
 import de.symeda.sormas.backend.infrastructure.community.Community;
 import de.symeda.sormas.backend.infrastructure.district.District;
+import de.symeda.sormas.backend.infrastructure.facility.Facility;
 import de.symeda.sormas.backend.infrastructure.region.Region;
 import de.symeda.sormas.backend.util.IterableHelper;
 import de.symeda.sormas.backend.util.ModelConstants;
@@ -423,6 +420,22 @@ public class UserService extends AdoServiceWithUserFilter<User> {
 
 		return password;
 	}
+
+	public String generatePassword() {
+		return PasswordHelper.createPass(12);
+	}
+
+	public String updatePassword(String userUuid, String password) {
+		User user = getByUuid(userUuid);
+
+		if (user == null && password == null) {
+			return null;
+		}
+		assert user != null;
+		user.setPassword(PasswordHelper.encodePassword(password, user.getSeed()));
+		ensurePersisted(user);
+		return password;
+	};
 
 	public Predicate buildCriteriaFilter(UserCriteria userCriteria, CriteriaBuilder cb, Root<User> from) {
 
