325a6cc80a216f6a8325dec642ebfb04f5943aa3
NORTH <yormen1@gmail.com>
2022-03-15 12:21:21 +0000

    Added the cadre reference

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
@@ -19,6 +19,7 @@ public interface Strings {
 	String between = "between";
 	String bpm = "bpm";
 	String by = "by";
+	String cadre = "cadre";
 	String classificationAllOf = "classificationAllOf";
 	String classificationClassificationRules = "classificationClassificationRules";
 	String classificationConfirmed = "classificationConfirmed";
