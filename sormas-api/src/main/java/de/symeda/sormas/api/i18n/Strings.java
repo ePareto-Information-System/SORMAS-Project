f4f38ebcc4d04f9fc2ae8ba8d2d7caff4814b456
Anthony4m <amoahanthony41@gmail.com>
2022-05-20 14:14:56 +0000

    apparently, contacts depend on cases.

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
@@ -1344,6 +1344,7 @@ public interface Strings {
 	String promptSamplesSearchField = "promptSamplesSearchField";
 	String promptSearch = "promptSearch";
 	String promptSelectPeriod = "promptSelectPeriod";
+<<<<<<< HEAD
 	String promptTaskDateFrom = "promptTaskDateFrom";
 	String promptTaskDateTo = "promptTaskDateTo";
 	String promptTaskDateType = "promptTaskDateType";
@@ -1355,6 +1356,8 @@ public interface Strings {
 	String promptTravelEntryEpiWeekFrom = "promptTravelEntryEpiWeekFrom";
 	String promptTravelEntryEpiWeekTo = "promptTravelEntryEpiWeekTo";
 	String promptTravelEntrySearchField = "promptTravelEntrySearchField";
+=======
+>>>>>>> 44a439af3a (add placeholders to fields)
 	String promptTreatmentTextFilter = "promptTreatmentTextFilter";
 	String promptTypeToAdd = "promptTypeToAdd";
 	String promptUserSearch = "promptUserSearch";
