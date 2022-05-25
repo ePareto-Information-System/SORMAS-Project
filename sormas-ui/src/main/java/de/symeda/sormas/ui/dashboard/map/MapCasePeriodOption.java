8f964802a0f5055989067509a43865b65ad5a135
Anthony4m <amoahanthony41@gmail.com>
2022-05-25 15:19:51 +0000

    Created enums for new cases and cummlative cases

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/map/MapCasePeriodOption.java
@@ -0,0 +1,13 @@
+package de.symeda.sormas.ui.dashboard.map;
+
+import de.symeda.sormas.api.i18n.I18nProperties;
+
+public enum MapCasePeriodOption {
+
+	NEW_CASES,
+	CASES_INCIDENCE;
+
+	public String toString() {
+		return I18nProperties.getEnumCaption(this);
+	}
+}
