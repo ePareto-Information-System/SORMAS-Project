3abac50b04f566de29fdae620754fc0e477fd0b2
Anthony4m <amoahanthony41@gmail.com>
2022-08-15 15:52:04 +0000

    set testresultverified to false if user is a labuser

--- sormas-api/src/main/java/de/symeda/sormas/api/sample/PathogenTestDto.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/sample/PathogenTestDto.java
@@ -1,17 +1,14 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
  * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -31,6 +28,7 @@ import de.symeda.sormas.api.sormastosormas.S2SIgnoreProperty;
 import de.symeda.sormas.api.sormastosormas.SormasToSormasConfig;
 import de.symeda.sormas.api.user.UserDto;
 import de.symeda.sormas.api.user.UserReferenceDto;
+import de.symeda.sormas.api.user.UserRole;
 import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.api.utils.DateFormatHelper;
 import de.symeda.sormas.api.utils.FieldConstraints;
@@ -132,6 +130,9 @@ public class PathogenTestDto extends PseudonymizableDto {
 		pathogenTest.setSample(sample.toReference());
 		if (sample.getSamplePurpose() == SamplePurpose.INTERNAL) {
 			pathogenTest.setTestResultVerified(true);
+		}
+		if (currentUser.getUserRoles().contains(UserRole.LAB_USER)) {
+			pathogenTest.setTestResultVerified(false);
 		}
 		pathogenTest.setLab(currentUser.getLaboratory());
 		if (pathogenTest.getLab() == null) {
