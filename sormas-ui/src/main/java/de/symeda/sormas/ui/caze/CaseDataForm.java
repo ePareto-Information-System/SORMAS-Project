37b20e65137b2660260c94bf7b479a3a4fea1e8e
Flava177 <flavaflav177@gmail.com>
2024-04-29 13:16:36 +0000

    removed info loc for for Monkey Pox

--- sormas-ui/src/main/java/de/symeda/sormas/ui/caze/CaseDataForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/caze/CaseDataForm.java
@@ -1325,6 +1325,19 @@ public class CaseDataForm extends AbstractEditForm<CaseDataDto> {
 					break;
 				}
 			}
+
+			// Automatic case classification rules button - invisible for other diseases
+			DiseaseClassificationCriteriaDto diseaseClassificationCriteria = FacadeProvider.getCaseClassificationFacade().getByDisease(disease);
+			if (diseaseClassificationExists()) {
+				Button classificationRulesButton = ButtonHelper.createIconButton(
+						Captions.info,
+						VaadinIcons.INFO_CIRCLE,
+						e -> ControllerProvider.getCaseController().openClassificationRulesPopup(diseaseClassificationCriteria),
+						ValoTheme.BUTTON_PRIMARY,
+						FORCE_CAPTION);
+
+				getContent().addComponent(classificationRulesButton, CLASSIFICATION_RULES_LOC);
+			}
 		}
 
 		if (!shouldHidePaperFormDates()) {
@@ -1334,18 +1347,6 @@ public class CaseDataForm extends AbstractEditForm<CaseDataDto> {
 
 		}
 
-		// Automatic case classification rules button - invisible for other diseases
-		DiseaseClassificationCriteriaDto diseaseClassificationCriteria = FacadeProvider.getCaseClassificationFacade().getByDisease(disease);
-		if (diseaseClassificationExists()) {
-			Button classificationRulesButton = ButtonHelper.createIconButton(
-					Captions.info,
-					VaadinIcons.INFO_CIRCLE,
-					e -> ControllerProvider.getCaseController().openClassificationRulesPopup(diseaseClassificationCriteria),
-					ValoTheme.BUTTON_PRIMARY,
-					FORCE_CAPTION);
-
-			getContent().addComponent(classificationRulesButton, CLASSIFICATION_RULES_LOC);
-		}
 
 		addField(CaseDataDto.DELETION_REASON);
 		addField(CaseDataDto.OTHER_DELETION_REASON, TextArea.class).setRows(3);
