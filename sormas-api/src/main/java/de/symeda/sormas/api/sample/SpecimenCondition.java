ed0556626a3de02f25f3c528662d12551b088bfc
douglasderrick <douglasder12@gmail.com>
2024-05-10 15:28:34 +0000

    Added Rejected to Specimen Condition

--- sormas-api/src/main/java/de/symeda/sormas/api/sample/SpecimenCondition.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/sample/SpecimenCondition.java
@@ -22,7 +22,8 @@ import de.symeda.sormas.api.i18n.I18nProperties;
 public enum SpecimenCondition {
 
 	ADEQUATE,
-	NOT_ADEQUATE;
+	NOT_ADEQUATE,
+	REJECTED;
 
 	@Override
 	public String toString() {
