e64ead45ab95e065c23d5bcbc8af586c8c33ae6d
Flava177 <flavaflav177@gmail.com>
2024-05-10 09:22:38 +0000

    Updated components for hospitalization for IDSR

--- sormas-ui/src/main/java/de/symeda/sormas/ui/hospitalization/HospitalizationForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/hospitalization/HospitalizationForm.java
@@ -473,7 +473,6 @@ public class HospitalizationForm extends AbstractEditForm<HospitalizationDto> {
 			hospitalizationReason.setVisible(false);
 			hospitalizedPreviouslyField.setVisible(false);
 			previousHospitalizationsHeadingLabel.setVisible(false);
-//			dateFormSentToDistrict.setVisible(false);
 		}
 
 		if(caze.getDisease() == Disease.NEW_INFLUENZA || caze.getDisease() == Disease.SARI){
@@ -481,11 +480,6 @@ public class HospitalizationForm extends AbstractEditForm<HospitalizationDto> {
 			hospitalizationReason.setVisible(false);
 			hospitalizedPreviouslyField.setVisible(false);
 			previousHospitalizationsHeadingLabel.setVisible(false);
-		/*	leftAgainstAdviceField.setVisible(false);
-			isolatedField.setVisible(false);
-			descriptionField.setVisible(false);
-			notifyDistrictDate.setVisible(false);
-			dateFormSentToDistrict.setVisible(false);*/
 
 			admittedToHealthFacilityFieldNew.setVisible(true);
 			admissionDateField.setVisible(true);
@@ -550,6 +544,13 @@ public class HospitalizationForm extends AbstractEditForm<HospitalizationDto> {
 		if (caze.getDisease() == Disease.CHOLERA){
 			hideAllFields();
 			setVisible(true, HospitalizationDto.HOSPITAL_RECORD_NUMBER);
+		}
+
+		if(caze.getDisease() == Disease.IMMEDIATE_CASE_BASED_FORM_OTHER_CONDITIONS){
+			setVisible(true, HospitalizationDto.ADMITTED_TO_HEALTH_FACILITY_NEW);
+			admittedToHealthFacilityFieldNew.setCaption("In-patient or Out-patient?");
+			dateFirstSeen.setVisible(true);
+			dateFirstSeen.setCaption("Date seen at health facility");
 		}
 	}
 
