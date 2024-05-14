ffd74e30714d64aa99f6fd20cda5edfdd2821305
douglasderrick <douglasder12@gmail.com>
2024-05-14 10:21:59 +0000

    Fixed virusDetectionGenotype caption issue in pathogen test

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
@@ -1769,7 +1769,7 @@ public interface Captions {
 	String PathogenTest_testTypeText = "PathogenTest.testTypeText";
 	String PathogenTest_typingId = "PathogenTest.typingId";
 	String PathogenTest_viaLims = "PathogenTest.viaLims";
-	String PathogenTest_virusDetectionGenotypeField = "PathogenTest.virusDetectionGenotypeField";
+	String PathogenTest_virusDetectionGenotype = "PathogenTest.virusDetectionGenotype";
 	String pathogenTestAdd = "pathogenTestAdd";
 	String pathogenTestCreateNew = "pathogenTestCreateNew";
 	String pathogenTestNewResult = "pathogenTestNewResult";
