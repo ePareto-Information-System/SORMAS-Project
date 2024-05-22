80c286b29ca1046475d6cd695c5f283c9b0d9985
Anthony4m <amoahanthony41@gmail.com>
2024-05-22 09:15:03 +0000

    added alert facade and service

---
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/ebs/EbsAlertFacadeEjb.java
@@ -0,0 +1,73 @@
+package de.symeda.sormas.backend.ebs;
+
+import de.symeda.sormas.api.ebs.EbsAlertDto;
+import de.symeda.sormas.api.ebs.EbsAlertFacade;
+import de.symeda.sormas.api.ebs.RiskAssessmentCriteria;
+import de.symeda.sormas.api.ebs.RiskAssessmentDto;
+import de.symeda.sormas.backend.util.DtoHelper;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import java.util.List;
+import java.util.stream.Collectors;
+@Stateless(name = "EbsAlertFacade")
+public class EbsAlertFacadeEjb implements EbsAlertFacade {
+    @EJB
+    private EbsService ebsService;
+    @EJB
+    private EbsAlertService ebsAlertService;
+
+    public EbsAlert fillOrBuildEntity(EbsAlertDto source, EbsAlert target, boolean checkChangeDate) {
+        if (source == null) {
+            return null;
+        }
+
+        target = DtoHelper.fillOrBuildEntity(source, target, EbsAlert::new, checkChangeDate);
+        target.setActionInitiated(source.getActionInitiated());
+        target.setResponseDate(source.getResponseDate());
+        target.setResponseStatus(source.getResponseStatus());
+        target.setAlertUsed(source.getAlertUsed());
+        target.setDetailsAlertUsed(source.getDetailsAlertUsed());
+        target.setDetailsResponseActivities(source.getDetailsResponseActivities());
+        target.setDetailsGiven(source.getDetailsGiven());
+        target.setEbs(ebsService.getByReferenceDto(source.getEbs()));
+        return target;
+    }
+
+    public EbsAlertDto toDto(EbsAlert ebsAlert) {
+        if (ebsAlert == null) {
+            return null;
+        }
+        EbsAlertDto target = new EbsAlertDto();
+        EbsAlert source = ebsAlert;
+        DtoHelper.fillDto(target, source);
+        target.setActionInitiated(source.getActionInitiated());
+        target.setResponseDate(source.getResponseDate());
+        target.setResponseStatus(source.getResponseStatus());
+        target.setAlertUsed(source.getAlertUsed());
+        target.setDetailsAlertUsed(source.getDetailsAlertUsed());
+        target.setDetailsResponseActivities(source.getDetailsResponseActivities());
+        target.setDetailsGiven(source.getDetailsGiven());
+        target.setEbs(EbsFacadeEjb.toReferenceDto(source.getEbs()));
+        return target;
+    }
+
+    public EbsAlertDto saveAlert(EbsAlertDto dto) {
+        EbsAlert ebsAlert = ebsAlertService.getByUuid(dto.getUuid());
+
+        EbsAlert ado = fillOrBuildEntity(dto, ebsAlert, true);
+        ebsAlertService.ensurePersisted(ado);
+        return toDto(ado);
+    }
+
+    @Override
+    public List<EbsAlertDto> findBy(RiskAssessmentCriteria criteria) {
+        return ebsAlertService.findBy(criteria).stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    @LocalBean
+    @Stateless
+    public static class EbsAlertFacadeEjbLocal extends EbsAlertFacadeEjb {
+    }
+}
---
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/ebs/EbsAlertService.java
@@ -0,0 +1,69 @@
+package de.symeda.sormas.backend.ebs;
+
+import de.symeda.sormas.api.ebs.RiskAssessmentCriteria;
+import de.symeda.sormas.api.utils.DataHelper;
+import de.symeda.sormas.backend.common.AbstractDomainObject;
+import de.symeda.sormas.backend.common.BaseAdoService;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.criteria.*;
+import java.util.Collections;
+import java.util.List;
+
+@Stateless
+@LocalBean
+public class EbsAlertService extends BaseAdoService<EbsAlert> {
+
+    public EbsAlertService() {super(EbsAlert.class);}
+
+    public EbsAlert createAlert(){
+        EbsAlert ebsAlert = new EbsAlert();
+        ebsAlert.setUuid(DataHelper.createUuid());
+        return ebsAlert;
+    }
+
+    public Predicate createDefaultFilter(CriteriaBuilder cb, Root<EbsAlert> root) {
+        return cb.isFalse(root.get(EbsAlert.DELETED));
+    }
+
+    public List<EbsAlert> getByEbsUuids(List<String> ebsUuids) {
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<EbsAlert> cq = cb.createQuery(EbsAlert.class);
+        Root<EbsAlert> riskRoot = cq.from(EbsAlert.class);
+        Join<EbsAlert, Ebs> ebsJoin = riskRoot.join(EbsAlert.EBS, JoinType.LEFT);
+
+        Predicate filter = cb.and(createDefaultFilter(cb, riskRoot), ebsJoin.get(AbstractDomainObject.UUID).in(ebsUuids));
+
+        cq.where(filter);
+        return em.createQuery(cq).getResultList();
+    }
+
+    public List<EbsAlert> findBy(RiskAssessmentCriteria criteria) {
+        if (criteria == null) {
+            throw new IllegalArgumentException("Criteria cannot be null");
+        }
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<EbsAlert> cq = cb.createQuery(EbsAlert.class);
+        Root<EbsAlert> root = cq.from(EbsAlert.class);
+        cq.select(root);
+
+        Predicate filter = cb.conjunction();  // Create a default true predicate
+        if (criteria.getEbs() != null && criteria.getEbs().getUuid() != null) {
+            filter = cb.and(filter, cb.equal(root.get("ebs").get("uuid"), criteria.getEbs().getUuid()));
+        }
+
+        cq.where(filter);
+        List<EbsAlert> result;
+        try {
+            result = em.createQuery(cq).getResultList();
+        } catch (Exception e) {
+            // Log the exception and handle it as needed
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+
+        return result;
+    }
+}
