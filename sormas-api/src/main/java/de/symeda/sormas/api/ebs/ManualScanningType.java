5de4682c06d442f0dcab03fb03de4c8f0f23241a
Anthony4m <amoahanthony41@gmail.com>
2024-05-23 16:45:58 +0000

    Updated ManualScanningType

--- sormas-api/src/main/java/de/symeda/sormas/api/ebs/ManualScanningType.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/ebs/ManualScanningType.java
@@ -5,8 +5,10 @@ import de.symeda.sormas.api.i18n.I18nProperties;
 public enum ManualScanningType {
     TV,
     RADIO,
+    COMMUNITY_INFORMANT,
     NEWSPAPER,
     ONLINE,
+    SOCIAL_MEDIA,
     OTHER;
     @Override
     public String toString() {
