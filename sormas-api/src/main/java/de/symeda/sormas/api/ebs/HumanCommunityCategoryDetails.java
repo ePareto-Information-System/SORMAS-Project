13f4c7a99068fb67d798d4a6bc77c2bd8b0303d9
Anthony4m <amoahanthony41@gmail.com>
2024-05-23 16:42:49 +0000

    updated CategoryDetails.java to HumanCommunityCategoryDetails

--- sormas-api/src/main/java/de/symeda/sormas/api/ebs/HumanCommunityCategoryDetails.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/ebs/HumanCommunityCategoryDetails.java
@@ -2,14 +2,13 @@ package de.symeda.sormas.api.ebs;
 
 import de.symeda.sormas.api.i18n.I18nProperties;
 
-public enum CategoryDetails {
-    RESPIRATORY_SYSMPTOMS,
+public enum HumanCommunityCategoryDetails {
+    RESPIRATORY_SYMPTOMS,
     SEVERE_ILLNESS,
     SEVERE_DIARRHOEA,
     ORIFICES,
     UNABLE_TO_WALK,
     OTC,
-    PREGNANT_WOMAN,
     HANGING_WORM;
     @Override
     public String toString() {
