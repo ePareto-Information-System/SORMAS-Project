1df83a3f1a244763aa7ca925852293beee7d1c29
Anthony4m <amoahanthony41@gmail.com>
2024-06-06 10:17:15 +0000

    Added captions and their definitions

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
@@ -1319,6 +1319,8 @@ public interface Captions {
 	String eventLinkToEventsWithinTheSameFacility = "eventLinkToEventsWithinTheSameFacility";
 	String eventNewEvent = "eventNewEvent";
 	String ebsNewEbs = "ebsNewEbs";
+	String ebsRiskAssessmentDisabled = "ebsRiskAssessmentDisabled";
+	String ebsAlertDisabled = "ebsAlertDisabled";
 	String ebsCreatEbs = "ebsCreatEbs";
 	String ebsAlert = "ebsAlert";
 	String ebsSignalView = "ebsSignalView";
@@ -1420,7 +1422,7 @@ public interface Captions {
 	String Ebs_verified= "Ebs.verified";
 	String Ebs_cases= "Ebs.cases";
 	String Ebs_death= "Ebs.death";
-	String Triaging_earlyWarning = "Triaging.earlyWarning";
+	String Triaging_potentialRisk = "Triaging.potentialRisk";
 	String Triaging_categoryDetails = "Triaging.categoryDetails";
 	String Triaging_specificSignal = "Triaging.specificSignal";
 	String Triaging_triagingDecision = "Triaging.triagingDecision";
@@ -1438,7 +1440,6 @@ public interface Captions {
 	String Triaging_decisionDate = "Triaging.decisionDate";
 	String Triaging_referredTo = "Triaging.referredTo";
 	String Triaging_outcomeSupervisor = "Triaging.outcomeSupervisor";
-	String Triaging_notSignal = "Triaging.notSignal";
 	String Triaging_humanCommunityCategoryDetails = "Triaging.humanCommunityCategoryDetails";
 	String Triaging_humanFacilityCategoryDetails = "Triaging.humanFacilityCategoryDetails";
 	String Triaging_humanLaboratoryCategoryDetails = "Triaging.humanLaboratoryCategoryDetails";
