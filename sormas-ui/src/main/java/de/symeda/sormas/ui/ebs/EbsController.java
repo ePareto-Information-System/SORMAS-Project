c8eb872dd6bb0369a10598587cbe8c5f18e76a3b
Anthony4m <amoahanthony41@gmail.com>
2024-06-06 15:05:21 +0000

    refactored code

--- sormas-ui/src/main/java/de/symeda/sormas/ui/ebs/EbsController.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/ebs/EbsController.java
@@ -253,7 +253,7 @@ public class EbsController {
 			FacadeProvider.getRiskAssessmentFacade().saveRisk(riskAssessmentDto);
 			FacadeProvider.getEbsFacade().save(ebs);
 			SormasUI.refreshView();
-			Notification.show(I18nProperties.getString(Strings.messageRiskAssessmentSavedShort), ERROR_MESSAGE);
+			Notification.show(I18nProperties.getString(Strings.messageRiskAssessmentSavedShort), TRAY_NOTIFICATION);
 			showAssessmentCaseDialog(riskAssessmentDto);
 			navigateToAlert(ebs.getUuid(),false);
 		});
