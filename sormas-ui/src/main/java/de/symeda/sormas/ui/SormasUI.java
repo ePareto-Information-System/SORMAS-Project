f926b5511783d34eb2ae4c3dddb866eb960916bc
douglasderrick <douglasder12@gmail.com>
2024-06-14 11:28:03 +0000

    Implemented auto navigate after save

--- sormas-ui/src/main/java/de/symeda/sormas/ui/SormasUI.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/SormasUI.java
@@ -29,8 +29,10 @@ import com.vaadin.ui.JavaScript;
 import com.vaadin.ui.JavaScriptFunction;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
+import de.symeda.sormas.api.Disease;
 import de.symeda.sormas.api.FacadeProvider;
 import de.symeda.sormas.api.auditlog.ChangeType;
+import de.symeda.sormas.api.feature.FeatureType;
 import de.symeda.sormas.api.user.UserRight;
 import de.symeda.sormas.ui.UserProvider.HasUserProvider;
 import de.symeda.sormas.ui.ViewModelProviders.HasViewModelProviders;
@@ -41,6 +43,8 @@ import elemental.json.JsonObject;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Main UI class of the application that shows either the login screen or the
@@ -58,6 +62,21 @@ public class SormasUI extends UI implements HasUserProvider, HasViewModelProvide
 
 	private final UserProvider userProvider = new UserProvider();
 	private final ViewModelProviders viewModelProviders = new ViewModelProviders();
+	private final static  String[][] caseBasedDiseases = {
+			{Disease.YELLOW_FEVER.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.AFP.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "60-Days", "immunizations", "samples"},
+			{Disease.AHF.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.CSM.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "contacts", "samples"},
+			{Disease.MEASLES.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.CHOLERA.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "riskFactor", "samples"},
+			{Disease.CORONAVIRUS.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "therapy", "clinicalcourse", "visits", "contacts", "samples"},
+			{Disease.GUINEA_WORM.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.IMMEDIATE_CASE_BASED_FORM_OTHER_CONDITIONS.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.NEW_INFLUENZA.getName(), "data", "person", "hospitalization", "symptoms", "epidata", "samples"},
+			{Disease.NEONATAL_TETANUS.getName(), "data", "person", "hospitalization", "symptoms"},
+			{Disease.FOODBORNE_ILLNESS.getName(), "data", "person", "symptoms", "epidata", "foodhistory", "60-Days"},
+			{Disease.MONKEYPOX.getName(), "data", "person", "hospitalization", "symptoms", "riskFactor"},
+	};
 
 	@Override
 	public void init(VaadinRequest vaadinRequest) {
@@ -105,6 +124,110 @@ public class SormasUI extends UI implements HasUserProvider, HasViewModelProvide
 
 	public static void refreshView() {
 		get().getNavigator().navigateTo(get().getNavigator().getState());
+	}
+
+	public static void navigateToCaseChild() {
+		String currentState = getCurrent().getNavigator().getState();
+		String caseUuid = extractCaseId(currentState);
+
+		String nextViewName = getNextViewName(caseUuid);
+		if (nextViewName.isEmpty()) {
+			return;
+		}
+		String navigationState = "cases/" + nextViewName + "/" + caseUuid;
+		get().getNavigator().navigateTo(navigationState);
+	}
+
+	public static String getNextViewName(String caseUuid) {
+
+		String currentState = getCurrent().getNavigator().getState();
+		String path = removeQueryParameters(currentState);
+		String currentLocation = "";
+		currentLocation = path.substring(path.indexOf("/") + 1, path.lastIndexOf("/"));
+		Disease currentDisease = FacadeProvider.getCaseFacade().getByUuid(caseUuid).getDisease();
+		if (currentDisease == null) {
+			return "";
+		}
+
+
+		String[] result = getSubArrayByFirstElement(currentDisease.getName());
+		if (result == null) {
+			return "";
+		}
+		result = java.util.Arrays.stream(result).filter(e -> hasPermission(caseUuid, e)).toArray(String[]::new);
+
+		String nextViewName = "";
+		for (int i = 0; i < result.length; i++) {
+			if (result[i].equals(currentLocation)) {
+				if (i + 1 < result.length) {
+					nextViewName = result[i + 1];
+				}
+				break;
+			}
+		}
+
+		return nextViewName;
+	}
+
+	public static boolean hasPermission(String caseUuid, String nextViewName) {
+		UserProvider userProvider = UserProvider.getCurrent();
+		switch (nextViewName) {
+			case "contacts":
+				if (!userProvider.hasUserRight(UserRight.CONTACT_VIEW)) {
+					return false;
+				}
+				break;
+			case "samples":
+				if (!FacadeProvider.getFeatureConfigurationFacade().isFeatureEnabled(FeatureType.SAMPLES_LAB) || !userProvider.hasUserRight(UserRight.SAMPLE_VIEW) || !userProvider.hasUserRight(UserRight.SAMPLE_EDIT)) {
+					return false;
+				}
+				break;
+			case "visits":
+				if (!FacadeProvider.getFeatureConfigurationFacade().isFeatureEnabled(FeatureType.CASE_FOLLOWUP)) {
+					return false;
+				}
+				break;
+			case "therapy":
+				if (!userProvider.hasUserRight(UserRight.THERAPY_VIEW)) {
+					return false;
+				}
+				break;
+			case "clinicalcourse":
+				if (!userProvider.hasUserRight(UserRight.CLINICAL_COURSE_VIEW)) {
+					return false;
+				}
+				break;
+			default:
+				break;
+		}
+		return true;
+	}
+
+	public static String[] getSubArrayByFirstElement(String firstElement) {
+		for (String[] array : caseBasedDiseases) {
+			if (array.length > 0 && array[0].equals(firstElement)) {
+				return array;
+			}
+		}
+		return null; // or you could throw an exception or return an empty array
+	}
+
+	public static String extractCaseId(String query) {
+		query = removeQueryParameters(query);
+		String caseUuid = query.substring(query.lastIndexOf("/") + 1);
+		return caseUuid;
+	}
+
+	//remove query parameters from the URL
+	public static String removeQueryParameters(String query) {
+		int queryStart = query.indexOf("?");
+		if (queryStart != -1) {
+			query = query.substring(0, queryStart);
+		}
+		if (query.endsWith("/")) {
+			query = query.substring(0, query.length() - 1);
+		}
+		return query;
 	}
 	
 	private void initUserActivityLogging () {
