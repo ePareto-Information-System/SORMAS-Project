2b56301b9a68753348f18ec47624c8b233ac6900
douglasderrick <douglasder12@gmail.com>
2024-06-18 14:16:48 +0000

    Hide village if Disease is Cholera

--- sormas-ui/src/main/java/de/symeda/sormas/ui/location/LocationEditForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/location/LocationEditForm.java
@@ -1020,7 +1020,7 @@ public class LocationEditForm extends AbstractEditForm<LocationDto> {
 	//handle CHOLERA
 	public void handleForCholera() {
 		setVisible(true, LocationDto.POSTAL_CODE, LocationDto.VILLAGE);
-		setVisible(false, LocationDto.STREET, LocationDto.ADDITIONAL_INFORMATION, LocationDto.CITY, LocationDto.HOUSE_NUMBER, LocationDto.AREA_TYPE);
+		setVisible(false, LocationDto.STREET, LocationDto.ADDITIONAL_INFORMATION, LocationDto.CITY, LocationDto.HOUSE_NUMBER, LocationDto.AREA_TYPE, LocationDto.VILLAGE);
 	}
 	
 	public void handleMeasles() {
