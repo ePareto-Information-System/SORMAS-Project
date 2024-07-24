3c57e19b33ae4a13fc0ae78fa193600e955c8a62
Flava177 <flavaflav177@gmail.com>
2024-07-24 09:40:23 +0000

    when Yes/No is not selected (i.e. at a default state) nothing should show for daytime fields

--- sormas-ui/src/main/java/de/symeda/sormas/ui/foodhistory/FoodHistoryForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/foodhistory/FoodHistoryForm.java
@@ -11,12 +11,17 @@ import de.symeda.sormas.api.epidata.EpiDataDto;
 import de.symeda.sormas.api.foodhistory.FoodHistoryDto;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.i18n.Strings;
+import de.symeda.sormas.api.utils.EventType;
+import de.symeda.sormas.api.utils.YesNo;
 import de.symeda.sormas.api.utils.fieldaccess.UiFieldAccessCheckers;
 import de.symeda.sormas.api.utils.fieldvisibility.FieldVisibilityCheckers;
 import de.symeda.sormas.ui.UserProvider;
 import de.symeda.sormas.ui.utils.AbstractEditForm;
 import de.symeda.sormas.ui.utils.DateTimeField;
+import de.symeda.sormas.ui.utils.FieldHelper;
 import de.symeda.sormas.ui.utils.NullableOptionGroup;
+
+import java.util.Arrays;
 
 import static de.symeda.sormas.ui.utils.CssStyles.H3;
 import static de.symeda.sormas.ui.utils.CssStyles.H4;
@@ -103,69 +108,92 @@ public class FoodHistoryForm extends AbstractEditForm<FoodHistoryDto> {
         addField(FoodHistoryDto.SUSPECTED_FOOD, TextField.class);
         addField(FoodHistoryDto.DATE_CONSUMED, DateTimeField.class);
         addField(FoodHistoryDto.FOOD_SOURCE, ComboBox.class);
-        addField(FoodHistoryDto.EVENT_TYPE, ComboBox.class);
-        addField(FoodHistoryDto.EVENT_OTHER_SPECIFY, TextField.class);
+        ComboBox eventType = addField(FoodHistoryDto.EVENT_TYPE, ComboBox.class);
+        TextField eventOtherSpecify = addField(FoodHistoryDto.EVENT_OTHER_SPECIFY, TextField.class);
 
-        addField(FoodHistoryDto.BREAKFAST, NullableOptionGroup.class);
+        NullableOptionGroup breakFast = addField(FoodHistoryDto.BREAKFAST, NullableOptionGroup.class);
         TextField totalNoPerson = addField(FoodHistoryDto.TOTAL_NO_PERSONS, TextField.class);
         totalNoPerson.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE, TextField.class);
-        addField(FoodHistoryDto.LUNCH, NullableOptionGroup.class);
+        TextField foodConsumed = addField(FoodHistoryDto.FOOD_CONSUMED, TextField.class);
+        TextField sourceOfFood = addField(FoodHistoryDto.SOURCE_OF_FOOD, TextField.class);
+        NullableOptionGroup consumedAtPlace = addField(FoodHistoryDto.CONSUMED_AT_PLACE, NullableOptionGroup.class);
+
+        NullableOptionGroup lunch = addField(FoodHistoryDto.LUNCH, NullableOptionGroup.class);
         TextField totalNoPersonl1 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_L1, TextField.class);
         totalNoPersonl1.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_L1, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD_L1, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_L1, TextField.class);
-        addField(FoodHistoryDto.SUPPER, NullableOptionGroup.class);
+        TextField foodConsumedL1 = addField(FoodHistoryDto.FOOD_CONSUMED_L1, TextField.class);
+        TextField sourceOfFoodL1 = addField(FoodHistoryDto.SOURCE_OF_FOOD_L1, TextField.class);
+        NullableOptionGroup consumedAtPlaceL1 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_L1, NullableOptionGroup.class);
+
+        NullableOptionGroup supper = addField(FoodHistoryDto.SUPPER, NullableOptionGroup.class);
         TextField totalNoPersons1 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_S1, TextField.class);
         totalNoPersons1.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_S1, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOODS_S1, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_S1, TextField.class);
+        TextField foodConsumedS1 = addField(FoodHistoryDto.FOOD_CONSUMED_S1, TextField.class);
+        TextField sourceOfFoodS1 = addField(FoodHistoryDto.SOURCE_OF_FOODS_S1, TextField.class);
+        NullableOptionGroup consumedAtPlaceS1 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_S1, NullableOptionGroup.class);
 
-        addField(FoodHistoryDto.BREAKFAST2, NullableOptionGroup.class);
+        setFieldsVisible(false, totalNoPerson, foodConsumed, sourceOfFood, consumedAtPlace, totalNoPersonl1, foodConsumedL1, sourceOfFoodL1, consumedAtPlaceL1, totalNoPersons1, foodConsumedS1, sourceOfFoodS1, consumedAtPlaceS1);
+        FieldHelper.setVisibleWhen(breakFast, Arrays.asList(totalNoPerson, foodConsumed, sourceOfFood, consumedAtPlace), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(lunch, Arrays.asList(totalNoPersonl1, foodConsumedL1, sourceOfFoodL1, consumedAtPlaceL1), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(supper, Arrays.asList(totalNoPersons1, foodConsumedS1, sourceOfFoodS1, consumedAtPlaceS1), Arrays.asList(YesNo.YES), true);
+
+        NullableOptionGroup breakfast2 = addField(FoodHistoryDto.BREAKFAST2, NullableOptionGroup.class);
         TextField totalNoPerson2 = addField(FoodHistoryDto.TOTAL_NO_PERSONS2, TextField.class);
         totalNoPerson2.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED2, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD2, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE2, TextField.class);
-        addField(FoodHistoryDto.LUNCH_L2, NullableOptionGroup.class);
+        TextField foodConsumedS2 = addField(FoodHistoryDto.FOOD_CONSUMED2, TextField.class);
+        TextField sourceOfFoodS2 = addField(FoodHistoryDto.SOURCE_OF_FOOD2, TextField.class);
+        NullableOptionGroup consumedAtPlaceS2 = addField(FoodHistoryDto.CONSUMED_AT_PLACE2, NullableOptionGroup.class);
+
+        NullableOptionGroup lunchl2 = addField(FoodHistoryDto.LUNCH_L2, NullableOptionGroup.class);
         TextField totalNoPersonl2 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_L2, TextField.class);
         totalNoPersonl2.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_L2, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD_L2, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_L2, TextField.class);
-        addField(FoodHistoryDto.SUPPER_S2, NullableOptionGroup.class);
+        TextField foodConsumedl2 = addField(FoodHistoryDto.FOOD_CONSUMED_L2, TextField.class);
+        TextField sourceOfFoodl2 = addField(FoodHistoryDto.SOURCE_OF_FOOD_L2, TextField.class);
+        NullableOptionGroup consumedAtPlacel2 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_L2, NullableOptionGroup.class);
+
+        NullableOptionGroup suppers2 = addField(FoodHistoryDto.SUPPER_S2, NullableOptionGroup.class);
         TextField totalNoPersonls2 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_S2, TextField.class);
         totalNoPersonls2.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_S2, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD_S2, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_S2, TextField.class);
+        TextField foodConsumeds2  = addField(FoodHistoryDto.FOOD_CONSUMED_S2, TextField.class);
+        TextField sourceOfFoods2 = addField(FoodHistoryDto.SOURCE_OF_FOOD_S2, TextField.class);
+        NullableOptionGroup consumedAtPlaces2 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_S2, NullableOptionGroup.class);
 
-        addField(FoodHistoryDto.BREAKFAST3, NullableOptionGroup.class);
+        setFieldsVisible(false, totalNoPerson2, foodConsumedS2, sourceOfFoodS2, consumedAtPlaceS2, totalNoPersonl2, foodConsumedl2, sourceOfFoodl2, consumedAtPlacel2, totalNoPersonls2, foodConsumeds2, sourceOfFoods2, consumedAtPlaces2);
+        FieldHelper.setVisibleWhen(breakfast2, Arrays.asList(totalNoPerson2, foodConsumedS2, sourceOfFoodS2, consumedAtPlaceS2), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(lunchl2, Arrays.asList(totalNoPersonl2, foodConsumedl2, sourceOfFoodl2, consumedAtPlacel2), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(suppers2, Arrays.asList(totalNoPersonls2, foodConsumeds2, sourceOfFoods2, consumedAtPlaces2), Arrays.asList(YesNo.YES), true);
+
+        NullableOptionGroup breakFast3 = addField(FoodHistoryDto.BREAKFAST3, NullableOptionGroup.class);
         TextField totalNoPerson3 = addField(FoodHistoryDto.TOTAL_NO_PERSONS3, TextField.class);
         totalNoPerson3.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED3, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD3, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE3, TextField.class);
-        addField(FoodHistoryDto.LUNCH_L3, NullableOptionGroup.class);
+        TextField foodConsumed3 = addField(FoodHistoryDto.FOOD_CONSUMED3, TextField.class);
+        TextField sourceOfFood3 = addField(FoodHistoryDto.SOURCE_OF_FOOD3, TextField.class);
+        NullableOptionGroup consumedAtPlaces3 = addField(FoodHistoryDto.CONSUMED_AT_PLACE3, NullableOptionGroup.class);
+
+        NullableOptionGroup lunchl3 = addField(FoodHistoryDto.LUNCH_L3, NullableOptionGroup.class);
         TextField totalNoPersonl3 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_L3, TextField.class);
         totalNoPersonl3.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_L3, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD_L3, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_L3, TextField.class);
-        addField(FoodHistoryDto.SUPPER_S3, NullableOptionGroup.class);
+        TextField foodConsumedl3 = addField(FoodHistoryDto.FOOD_CONSUMED_L3, TextField.class);
+        TextField sourceOfFoodl3 = addField(FoodHistoryDto.SOURCE_OF_FOOD_L3, TextField.class);
+        NullableOptionGroup consumedAtPlacesl3 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_L3, NullableOptionGroup.class);
+
+        NullableOptionGroup suppers3 = addField(FoodHistoryDto.SUPPER_S3, NullableOptionGroup.class);
         TextField totalNoPersons3 = addField(FoodHistoryDto.TOTAL_NO_PERSONS_S3, TextField.class);
         totalNoPersons3.addStyleName("v-captiontext-foodborne");
-        addField(FoodHistoryDto.FOOD_CONSUMED_S3, TextField.class);
-        addField(FoodHistoryDto.SOURCE_OF_FOOD_S3, TextField.class);
-        addField(FoodHistoryDto.CONSUMED_AT_PLACE_S3, TextField.class);
+        TextField foodConsumeds3 = addField(FoodHistoryDto.FOOD_CONSUMED_S3, TextField.class);
+        TextField sourceOfFoods3 = addField(FoodHistoryDto.SOURCE_OF_FOOD_S3, TextField.class);
+        NullableOptionGroup consumedAtPlacess3 = addField(FoodHistoryDto.CONSUMED_AT_PLACE_S3, NullableOptionGroup.class);
+
+        setFieldsVisible(false, totalNoPerson3, foodConsumed3, sourceOfFood3, consumedAtPlaces3, totalNoPersonl3, foodConsumedl3, sourceOfFoodl3, consumedAtPlacesl3, totalNoPersons3, foodConsumeds3, sourceOfFoods3, consumedAtPlacess3);
+        FieldHelper.setVisibleWhen(breakFast3, Arrays.asList(totalNoPerson3, foodConsumed3, sourceOfFood3, consumedAtPlaces3), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(lunchl3, Arrays.asList(totalNoPersonl3, foodConsumedl3, sourceOfFoodl3, consumedAtPlacesl3), Arrays.asList(YesNo.YES), true);
+        FieldHelper.setVisibleWhen(suppers3, Arrays.asList(totalNoPersons3, foodConsumeds3, sourceOfFoods3, consumedAtPlacess3), Arrays.asList(YesNo.YES), true);
 
         initializeVisibilitiesAndAllowedVisibilities();
         initializeAccessAndAllowedAccesses();
 
+        eventOtherSpecify.setVisible(false);
+        eventType.addValueChangeListener(valueChangeEvent -> eventOtherSpecify.setVisible(eventType.getValue() == EventType.OTHER));
     }
 
     private Label createLabel(String text, String h4, String location) {
