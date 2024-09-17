8ef6e74e497cdbd1842d135933171a025acdbde3
anthony4m <amoahanthony41@gmail.com>
2024-09-17 10:11:43 +0000

    created triaging  editfragment

---
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/triaging/edit/TriagingNewActivity.java
@@ -0,0 +1,186 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.symeda.sormas.app.triaging.edit;
+
+import static de.symeda.sormas.app.core.notification.NotificationType.ERROR;
+import static de.symeda.sormas.app.core.notification.NotificationType.WARNING;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.annotation.NonNull;
+
+import de.symeda.sormas.api.ebs.EbsTriagingDecision;
+import de.symeda.sormas.api.utils.ValidationException;
+import de.symeda.sormas.app.BaseEditActivity;
+import de.symeda.sormas.app.BaseEditFragment;
+import de.symeda.sormas.app.R;
+import de.symeda.sormas.app.backend.common.DaoException;
+import de.symeda.sormas.app.backend.common.DatabaseHelper;
+import de.symeda.sormas.app.backend.ebs.triaging.Triaging;
+import de.symeda.sormas.app.component.menu.PageMenuItem;
+import de.symeda.sormas.app.component.validation.FragmentValidator;
+import de.symeda.sormas.app.core.async.AsyncTaskResult;
+import de.symeda.sormas.app.core.async.SavingAsyncTask;
+import de.symeda.sormas.app.core.async.TaskResultHolder;
+import de.symeda.sormas.app.core.notification.NotificationHelper;
+import de.symeda.sormas.app.ebs.edit.TriagingEditFragment;
+import de.symeda.sormas.app.util.Bundler;
+
+public class TriagingNewActivity extends BaseEditActivity<Triaging> {
+
+	public static final String TAG = TriagingNewActivity.class.getSimpleName();
+
+	private AsyncTask saveTask;
+
+	private String ebsUuid;
+
+	public static void startActivity(Context fromActivity) {
+		BaseEditActivity.startActivity(fromActivity, TriagingNewActivity.class, buildBundle());
+	}
+
+	public static void startActivityFromEbs(Context fromActivity, String ebsUuid) {
+		BaseEditActivity.startActivity(fromActivity, TriagingNewActivity.class, buildBundleWithEbs(ebsUuid));
+	}
+
+	public static Bundler buildBundle() {
+		return BaseEditActivity.buildBundle(null);
+	}
+
+	public static Bundler buildBundleWithEbs(String ebsUuid) {
+		return BaseEditActivity.buildBundle(null).setEbsUuid(ebsUuid);
+	}
+
+	@Override
+	protected void onCreateInner(Bundle savedInstanceState) {
+		super.onCreateInner(savedInstanceState);
+		Bundler bundler = new Bundler(savedInstanceState);
+		ebsUuid = bundler.getEbsUuid();
+	}
+
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Bundler bundler = new Bundler(outState);
+		bundler.setCaseUuid(ebsUuid);
+	}
+
+	@Override
+	public EbsTriagingDecision getPageStatus() {
+		return null;
+	}
+
+	@Override
+	protected Triaging queryRootEntity(String recordUuid) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected Triaging buildRootEntity() {
+		Triaging triaging = DatabaseHelper.getTriagingDao().build();
+		return triaging;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean result = super.onCreateOptionsMenu(menu);
+		getSaveMenu().setTitle(R.string.action_save_task);
+		return result;
+	}
+
+	@Override
+	protected BaseEditFragment buildEditFragment(PageMenuItem menuItem, Triaging activityRootData) {
+		BaseEditFragment fragment = TriagingEditFragment.newInstance(activityRootData);
+		fragment.setLiveValidationDisabled(true);
+		return fragment;
+	}
+
+	@Override
+	protected int getActivityTitle() {
+		return R.string.action_new_triaging;
+	}
+
+	@Override
+	public void replaceFragment(BaseEditFragment f, boolean allowBackNavigation) {
+		super.replaceFragment(f, allowBackNavigation);
+		getActiveFragment().setLiveValidationDisabled(true);
+	}
+
+	@Override
+	public void saveData() {
+
+		if (saveTask != null) {
+			NotificationHelper.showNotification(this, WARNING, getString(R.string.message_already_saving));
+			return; // don't save multiple times
+		}
+
+		final Triaging taskToSave = getStoredRootEntity();
+		TriagingEditFragment fragment = (TriagingEditFragment) getActiveFragment();
+
+		fragment.setLiveValidationDisabled(false);
+
+		try {
+			FragmentValidator.validate(getContext(), fragment.getContentBinding());
+		} catch (ValidationException e) {
+			NotificationHelper.showNotification(this, ERROR, e.getMessage());
+			return;
+		}
+
+		saveDataInner(taskToSave);
+	}
+
+	private void saveDataInner(final Triaging taskToSave) {
+
+		if (saveTask != null) {
+			NotificationHelper.showNotification(this, WARNING, getString(R.string.message_already_saving));
+			return; // don't save multiple times
+		}
+
+		saveTask = new SavingAsyncTask(getRootView(), taskToSave) {
+
+			@Override
+			protected void onPreExecute() {
+				showPreloader();
+			}
+
+			@Override
+			public void doInBackground(TaskResultHolder resultHolder) throws DaoException, ValidationException {
+				DatabaseHelper.getTriagingDao().saveAndSnapshot(taskToSave);
+			}
+
+			@Override
+			protected void onPostExecute(AsyncTaskResult<TaskResultHolder> taskResult) {
+				hidePreloader();
+				super.onPostExecute(taskResult);
+				if (taskResult.getResultStatus().isSuccess()) {
+					finish();
+					TriagingEditActivity.startActivity(getContext(), taskToSave.getUuid());
+				}
+				saveTask = null;
+			}
+		}.executeOnThreadPool();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		if (saveTask != null && !saveTask.isCancelled())
+			saveTask.cancel(true);
+	}
+}
