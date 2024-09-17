74f07ade79a9164f90b177fa0f2798029c3660c6
anthony4m <amoahanthony41@gmail.com>
2024-09-17 09:55:52 +0000

    created ebs,triaging,ebsalert,riskassessment entities

---
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/backend/ebs/Ebs.java
@@ -0,0 +1,375 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2021 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.symeda.sormas.app.backend.ebs;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import de.symeda.sormas.api.ebs.AutomaticScanningType;
+import de.symeda.sormas.api.ebs.EbsSourceType;
+import de.symeda.sormas.api.ebs.ManualScanningType;
+import de.symeda.sormas.api.ebs.MediaScannningType;
+import de.symeda.sormas.api.ebs.PersonReporting;
+import de.symeda.sormas.app.backend.common.PseudonymizableAdo;
+import de.symeda.sormas.app.backend.location.Location;
+import de.symeda.sormas.app.backend.sormastosormas.SormasToSormasOriginInfo;
+import de.symeda.sormas.app.backend.ebs.triaging.Triaging;
+import de.symeda.sormas.app.backend.user.User;
+import de.symeda.sormas.app.backend.ebs.signalVerification.SignalVerification;
+
+@Entity(name = Ebs.TABLE_NAME)
+@DatabaseTable(tableName = Ebs.TABLE_NAME)
+public class Ebs extends PseudonymizableAdo {
+    public static final String SIGNAL_VERIFICATION = "signalVerification";
+    private static final long serialVersionUID = 4964495716032049582L;
+
+    public static final String TABLE_NAME = "ebs";
+    public static final String I18N_PREFIX = "Ebs";
+
+    @DatabaseField
+    private String informantName;
+
+    @DatabaseField
+    private String informantTel;
+
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date triageDate;
+
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date endDate;
+
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date reportDateTime;
+
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date dateOnset;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private User reportingUser;
+
+    @DatabaseField
+    @Enumerated(EnumType.STRING)
+    private EbsSourceType sourceInformation;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private User responsibleUser;
+
+    @DatabaseField
+    private String sourceName;
+
+    @DatabaseField
+    private String sourceUrl;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Location ebsLocation;
+
+    @DatabaseField
+    private Double ebsLongitude;
+
+    @DatabaseField
+    private Double ebsLatitude;
+
+    @DatabaseField
+    private Double ebsLatLon;
+
+    @DatabaseField
+    private String descriptionOccurrence;
+
+    @DatabaseField
+    private String personRegistering;
+
+    @DatabaseField
+    private String personDesignation;
+
+    @DatabaseField
+    private String personPhone;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private SormasToSormasOriginInfo sormasToSormasOriginInfo;
+
+    @DatabaseField
+    private String internalToken;
+
+    @DatabaseField
+    @Enumerated(EnumType.STRING)
+    private AutomaticScanningType automaticScanningType;
+
+    @DatabaseField
+    @Enumerated(EnumType.STRING)
+    private ManualScanningType manualScanningType;
+
+    @DatabaseField
+    @Enumerated(EnumType.STRING)
+    private MediaScannningType scanningType;
+
+    @DatabaseField
+    private String other;
+
+    @Enumerated(EnumType.STRING)
+    private PersonReporting categoryOfInformant;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private SignalVerification signalVerification;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Triaging triaging;
+
+    @DatabaseField
+    private String otherInformant;
+
+    public String getInformantName() {
+        return informantName;
+    }
+
+    public void setInformantName(String informantName) {
+        this.informantName = informantName;
+    }
+
+    public String getInformantTel() {
+        return informantTel;
+    }
+
+    public void setInformantTel(String informantTel) {
+        this.informantTel = informantTel;
+    }
+
+    public Date getTriageDate() {
+        return triageDate;
+    }
+
+    public void setTriageDate(Date triageDate) {
+        this.triageDate = triageDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getReportDateTime() {
+        return reportDateTime;
+    }
+
+    public void setReportDateTime(Date reportDateTime) {
+        this.reportDateTime = reportDateTime;
+    }
+
+    public Date getDateOnset() {
+        return dateOnset;
+    }
+
+    public void setDateOnset(Date dateOnset) {
+        this.dateOnset = dateOnset;
+    }
+
+    public User getReportingUser() {
+        return reportingUser;
+    }
+
+    public void setReportingUser(User reportingUser) {
+        this.reportingUser = reportingUser;
+    }
+
+    public EbsSourceType getSourceInformation() {
+        return sourceInformation;
+    }
+
+    public void setSourceInformation(EbsSourceType sourceInformation) {
+        this.sourceInformation = sourceInformation;
+    }
+
+    public User getResponsibleUser() {
+        return responsibleUser;
+    }
+
+    public void setResponsibleUser(User responsibleUser) {
+        this.responsibleUser = responsibleUser;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
+    public Location getEbsLocation() {
+        return ebsLocation;
+    }
+
+    public void setEbsLocation(Location ebsLocation) {
+        this.ebsLocation = ebsLocation;
+    }
+
+    public Double getEbsLongitude() {
+        return ebsLongitude;
+    }
+
+    public void setEbsLongitude(Double ebsLongitude) {
+        this.ebsLongitude = ebsLongitude;
+    }
+
+    public Double getEbsLatitude() {
+        return ebsLatitude;
+    }
+
+    public void setEbsLatitude(Double ebsLatitude) {
+        this.ebsLatitude = ebsLatitude;
+    }
+
+    public Double getEbsLatLon() {
+        return ebsLatLon;
+    }
+
+    public void setEbsLatLon(Double ebsLatLon) {
+        this.ebsLatLon = ebsLatLon;
+    }
+
+    public String getDescriptionOccurrence() {
+        return descriptionOccurrence;
+    }
+
+    public void setDescriptionOccurrence(String descriptionOccurrence) {
+        this.descriptionOccurrence = descriptionOccurrence;
+    }
+
+    public String getPersonRegistering() {
+        return personRegistering;
+    }
+
+    public void setPersonRegistering(String personRegistering) {
+        this.personRegistering = personRegistering;
+    }
+
+    public String getPersonDesignation() {
+        return personDesignation;
+    }
+
+    public void setPersonDesignation(String personDesignation) {
+        this.personDesignation = personDesignation;
+    }
+
+    public String getPersonPhone() {
+        return personPhone;
+    }
+
+    public void setPersonPhone(String personPhone) {
+        this.personPhone = personPhone;
+    }
+
+    public SormasToSormasOriginInfo getSormasToSormasOriginInfo() {
+        return sormasToSormasOriginInfo;
+    }
+
+    public void setSormasToSormasOriginInfo(SormasToSormasOriginInfo sormasToSormasOriginInfo) {
+        this.sormasToSormasOriginInfo = sormasToSormasOriginInfo;
+    }
+
+    public String getInternalToken() {
+        return internalToken;
+    }
+
+    public void setInternalToken(String internalToken) {
+        this.internalToken = internalToken;
+    }
+
+    public AutomaticScanningType getAutomaticScanningType() {
+        return automaticScanningType;
+    }
+
+    public void setAutomaticScanningType(AutomaticScanningType automaticScanningType) {
+        this.automaticScanningType = automaticScanningType;
+    }
+
+    public ManualScanningType getManualScanningType() {
+        return manualScanningType;
+    }
+
+    public void setManualScanningType(ManualScanningType manualScanningType) {
+        this.manualScanningType = manualScanningType;
+    }
+
+    public MediaScannningType getScanningType() {
+        return scanningType;
+    }
+
+    public void setScanningType(MediaScannningType scanningType) {
+        this.scanningType = scanningType;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+    public PersonReporting getCategoryOfInformant() {
+        return categoryOfInformant;
+    }
+
+    public void setCategoryOfInformant(PersonReporting categoryOfInformant) {
+        this.categoryOfInformant = categoryOfInformant;
+    }
+
+    public String getOtherInformant() {
+        return otherInformant;
+    }
+
+    public void setOtherInformant(String otherInformant) {
+        this.otherInformant = otherInformant;
+    }
+
+    public SignalVerification getSignalVerification() {
+        return signalVerification;
+    }
+
+    public void setSignalVerification(SignalVerification signalVerification) {
+        this.signalVerification = signalVerification;
+    }
+    @Override
+    public String getI18nPrefix() {
+        return I18N_PREFIX;
+    }
+
+    public Triaging getTriaging() {
+        return triaging;
+    }
+
+    public void setTriaging(Triaging triaging) {
+        this.triaging = triaging;
+    }
+}
---
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/backend/ebs/signalVerification/SignalVerification.java
@@ -0,0 +1,131 @@
+package de.symeda.sormas.app.backend.ebs.signalVerification;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import de.symeda.sormas.api.ebs.SignalOutcome;
+import de.symeda.sormas.api.utils.YesNo;
+import de.symeda.sormas.app.backend.common.PseudonymizableAdo;
+
+@Entity(name = SignalVerification.TABLE_NAME)
+@DatabaseTable(tableName = SignalVerification.TABLE_NAME)
+public class SignalVerification extends PseudonymizableAdo {
+    public static final String TABLE_NAME = "signalVerification";
+    public static final String I18N_PREFIX = "signalVerification";
+
+    private static final long serialVersionUID = 1L;
+    public static final long APPROXIMATE_JSON_SIZE_IN_BYTES = 13356;
+
+    @DatabaseField
+    private YesNo verificationSent;
+    @DatabaseField
+    private SignalOutcome verified;
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date verificationCompleteDate;
+    @DatabaseField(dataType = DataType.DATE_LONG)
+    private Date dateOfOccurrence;
+    @DatabaseField
+    private String numberOfPersonAnimal;
+    @DatabaseField
+    private String numberOfDeath;
+    @DatabaseField
+    private String numberOfPersonCases;
+    @DatabaseField
+    private String numberOfDeathPerson;
+    @DatabaseField
+    private String description;
+    @DatabaseField
+    private String whyNotVerify;
+
+    public YesNo getVerificationSent() {
+        return verificationSent;
+    }
+
+    public void setVerificationSent(YesNo verificationSent) {
+        this.verificationSent = verificationSent;
+    }
+    @Override
+    public String getI18nPrefix() {
+        return I18N_PREFIX;
+    }
+
+    @Enumerated(EnumType.STRING)
+    public SignalOutcome getVerified() {
+        return verified;
+    }
+
+    public void setVerified(SignalOutcome verified) {
+        this.verified = verified;
+    }
+
+    public Date getVerificationCompleteDate() {
+        return verificationCompleteDate;
+    }
+
+    public void setVerificationCompleteDate(Date verificationCompleteDate) {
+        this.verificationCompleteDate = verificationCompleteDate;
+    }
+
+    public Date getDateOfOccurrence() {
+        return dateOfOccurrence;
+    }
+
+    public void setDateOfOccurrence(Date dateOfOccurrence) {
+        this.dateOfOccurrence = dateOfOccurrence;
+    }
+
+    public String getNumberOfPersonAnimal() {
+        return numberOfPersonAnimal;
+    }
+
+    public void setNumberOfPersonAnimal(String numberOfPersonAnimal) {
+        this.numberOfPersonAnimal = numberOfPersonAnimal;
+    }
+
+    public String getNumberOfDeath() {
+        return numberOfDeath;
+    }
+
+    public void setNumberOfDeath(String numberOfDeath) {
+        this.numberOfDeath = numberOfDeath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWhyNotVerify() {
+        return whyNotVerify;
+    }
+
+    public void setWhyNotVerify(String whyNotVerify) {
+        this.whyNotVerify = whyNotVerify;
+    }
+
+    public String getNumberOfPersonCases() {
+        return numberOfPersonCases;
+    }
+
+    public void setNumberOfPersonCases(String numberOfPersonCases) {
+        this.numberOfPersonCases = numberOfPersonCases;
+    }
+
+    public String getNumberOfDeathPerson() {
+        return numberOfDeathPerson;
+    }
+
+    public void setNumberOfDeathPerson(String numberOfDeathPerson) {
+        this.numberOfDeathPerson = numberOfDeathPerson;
+    }
+}
