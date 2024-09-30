7b8b02ddbe53f53641c9e9512508483bb5ca1d9e
Flava177 <flavaflav177@gmail.com>
2024-09-30 06:08:53 +0000

    #158: Updated fragment_sample_edit_layout: rearranged the positioning of fields.

--- sormas-app/app/src/main/java/de/symeda/sormas/app/sample/edit/SampleEditFragment.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/sample/edit/SampleEditFragment.java
@@ -484,5 +484,7 @@ public class SampleEditFragment extends BaseEditFragment<FragmentSampleEditLayou
 		);
 		getContentBinding().samplePathogenTestResult.initializeSpinner(DataUtils.toItems(yellowFeverTestResults));
 		getContentBinding().samplePurpose.setValue(SamplePurpose.EXTERNAL);
+		getContentBinding().samplePurpose.setVisibility(GONE);
+		getContentBinding().samplePathogenTestingRequested.setVisibility(GONE);
 	}
 }
