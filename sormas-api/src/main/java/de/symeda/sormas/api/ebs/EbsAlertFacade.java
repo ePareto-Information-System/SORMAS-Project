531b4cfb2ca43ebe5061abef91d7ac40f6a7cd09
anthony4m <amoahanthony41@gmail.com>
2024-10-02 14:27:59 +0000

    Implemented the endpoints on the web for syncronizing

--- sormas-api/src/main/java/de/symeda/sormas/api/ebs/EbsAlertFacade.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/ebs/EbsAlertFacade.java
@@ -2,10 +2,14 @@ package de.symeda.sormas.api.ebs;
 
 import javax.ejb.Remote;
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 
 @Remote
 public interface EbsAlertFacade {
     EbsAlertDto saveAlert(@Valid EbsAlertDto dto);
     List<EbsAlertDto> findBy(RiskAssessmentCriteria criteria);
+    List<EbsAlertDto> getAllAfter(Date date);
+    EbsAlertDto getAlertByUuid(String uuid, boolean detailedReferences);
+    List<String> getAllActiveUuids();
 }
