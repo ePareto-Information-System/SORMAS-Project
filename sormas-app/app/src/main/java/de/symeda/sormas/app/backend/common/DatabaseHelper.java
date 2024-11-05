15573313e9de5002b8ad6400cdb9cac5e272efbb
anthony4m <amoahanthony41@gmail.com>
2024-11-05 17:14:10 +0000

    database helper changes

--- sormas-app/app/src/main/java/de/symeda/sormas/app/backend/common/DatabaseHelper.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/backend/common/DatabaseHelper.java
@@ -3187,6 +3187,8 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 									"categoryDetailsLevel varchar(255)," +
 									"supervisorreview varchar(3)," +
 									"referred varchar(3)" +
+									"ebs_id INTEGER NOT NULL," +
+									"FOREIGN KEY (ebs_id) REFERENCES ebs(id)" +
 									");"
 					);
 			case 346:
