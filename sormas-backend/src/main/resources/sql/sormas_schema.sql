2b1fb7b3aec6a3bef46dde9ee2e3c54ccae8f463
Flava177 <flavaflav177@gmail.com>
2024-11-11 12:03:24 +0000

    Updated sormas sql script to fix an issue with displayorder containing null values

--- sormas-backend/src/main/resources/sql/sormas_schema.sql
+++ sormas-backend/src/main/resources/sql/sormas_schema.sql
@@ -15125,27 +15125,32 @@ CREATE TRIGGER versioning_trigger
 ALTER TABLE forms_form_fields_history OWNER TO sormas_user;
 INSERT INTO schema_version (version_number, comment) VALUES (710, 'Added forms_form_fields table');
 
+--Add the displayOrder column as nullable first
 ALTER TABLE forms_form_fields ADD COLUMN displayOrder INTEGER;
-ALTER TABLE forms_form_fields ALTER COLUMN displayOrder SET NOT NULL;
-INSERT INTO schema_version (version_number, comment) VALUES (711, 'Added displayorder to forms_form_fields table');
+INSERT INTO schema_version (version_number, comment) VALUES (711, 'Added displayOrder column to forms_form_fields');
 
-CREATE INDEX idx_forms_form_fields_order
-    ON forms_form_fields (form_id, displayOrder);
-INSERT INTO schema_version (version_number, comment) VALUES (712, 'Craeted index for displayorder on forms_form_fields table');
-
--- Update any null displayOrder values with valid ordering
+--Set default values for displayOrder using a sequential order per form
 UPDATE forms_form_fields
 SET displayOrder = subquery.row_num - 1
     FROM (
-    SELECT form_id, formField_id, ROW_NUMBER() OVER (PARTITION BY form_id ORDER BY formField_id) as row_num
+    SELECT form_id, formField_id, ROW_NUMBER() OVER (PARTITION BY form_id ORDER BY formField_id) AS row_num
     FROM forms_form_fields
-    WHERE displayOrder IS NULL
-) as subquery
+) AS subquery
 WHERE forms_form_fields.form_id = subquery.form_id
-  AND forms_form_fields.formField_id = subquery.formField_id
-  AND forms_form_fields.displayOrder IS NULL;
+  AND forms_form_fields.formField_id = subquery.formField_id;
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM forms_form_fields WHERE displayOrder IS NULL) THEN
+        RAISE EXCEPTION 'There are still NULL values in displayOrder column';
+END IF;
+END $$;
+INSERT INTO schema_version (version_number, comment) VALUES (712, 'Update all records with the correct display order');
 
 ALTER TABLE forms_form_fields ALTER COLUMN displayOrder SET NOT NULL;
-INSERT INTO schema_version (version_number, comment) VALUES (713, 'Made updates on forms_form_fields table');
 
+--Create an index for ordering
+CREATE INDEX idx_forms_form_fields_order
+    ON forms_form_fields (form_id, displayOrder);
+INSERT INTO schema_version (version_number, comment) VALUES (713, 'Created index for displayorder on forms_form_fields table');
 -- *** Insert new sql commands BEFORE this line. Remember to always consider _history tables. ***
