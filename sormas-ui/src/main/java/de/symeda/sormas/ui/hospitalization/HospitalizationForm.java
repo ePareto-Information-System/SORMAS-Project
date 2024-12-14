c0c25775706fd3a595ba2b0edfafbead0f40bc5e
Flava177 <flavaflav177@gmail.com>
2024-12-14 12:19:24 +0000

    reverted changes for locationtype to it's default

--- sormas-ui/src/main/java/de/symeda/sormas/ui/hospitalization/HospitalizationForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/hospitalization/HospitalizationForm.java
@@ -627,58 +627,19 @@ public class HospitalizationForm extends AbstractEditForm<HospitalizationDto> {
 
 		}
 
-		if (caze.getDisease() == Disease.MONKEYPOX) {
-			setFieldsVisible(true, soughtRegion, soughtDistrict, soughtCommunity, nameOfFacilityField);
-
-			// Retrieve and set values for Monkeypox disease
-			RegionReferenceDto responsibleRegion = caze.getResponsibleRegion();
-			DistrictReferenceDto responsibleDistrict = caze.getResponsibleDistrict();
-			CommunityReferenceDto responsibleCommunity = caze.getResponsibleCommunity();
-			FacilityReferenceDto responsibleFacility = caze.getHealthFacility();
-
-			if (responsibleRegion != null) {
-				soughtRegion.setValue(responsibleRegion); // Set selected value
-				FieldHelper.updateItems(
-						soughtDistrict,
-						FacadeProvider.getDistrictFacade().getAllActiveByRegion(responsibleRegion.getUuid())
-				);
-			}
-
-			if (responsibleDistrict != null) {
-				soughtDistrict.setValue(responsibleDistrict); // Set selected value
-				FieldHelper.updateItems(
-						soughtCommunity,
-						FacadeProvider.getCommunityFacade().getAllActiveByDistrict(responsibleDistrict.getUuid())
-				);
-			}
-
-			if (responsibleCommunity != null) {
-				soughtCommunity.setValue(responsibleCommunity); // Set selected value
-				FieldHelper.updateItems(
-						nameOfFacilityField,
-						FacadeProvider.getFacilityFacade().getActiveHospitalsByCommunity(responsibleCommunity, true, true, true)
-				);
-			}
-
-			if (responsibleFacility != null) {
-				nameOfFacilityField.setValue(responsibleFacility); // Set selected value
-			}
-		}
-
-	/*	if(caze.getDisease() == Disease.MONKEYPOX){
-//			addField(HospitalizationDto.LOCATION_TYPE, addressForm);
-//			addressForm.setCaption(null);
+		if(caze.getDisease() == Disease.MONKEYPOX){
+			addField(HospitalizationDto.LOCATION_TYPE, addressForm);
+			addressForm.setCaption(null);
 			setFieldsVisible(true, admittedToHealthFacilityNew);
-			*//*FieldHelper.setVisibleWhen(
+			FieldHelper.setVisibleWhen(
 					getFieldGroup(),
 					Arrays.asList(HospitalizationDto.ADMISSION_DATE, HospitalizationDto.HOSPITAL_RECORD_NUMBER, HospitalizationDto.LOCATION_TYPE, HospitalizationDto.NAME_OF_FACILITY),
 					HospitalizationDto.ADMITTED_TO_HEALTH_FACILITY_NEW,
 					Arrays.asList(YesNo.YES),
 					false
-			);*//*
+			);
 
-			setFieldsVisible(true, soughtRegion, soughtDistrict, soughtCommunity, nameOfFacilityField);
-		}*/
+		}
 
 		if (caze.getDisease() == Disease.GUINEA_WORM) {
 			hideAllFields();
