7a7cab1f5e23ba334c1a9505375f9652fe12144e
Jude Clottey <judeclottey1@gmail.com>
2024-12-17 15:08:56 +0000

    fixed missiong subtitles for afp

--- sormas-app/app/src/main/java/de/symeda/sormas/app/sample/edit/SampleEditFragment.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/sample/edit/SampleEditFragment.java
@@ -20,7 +20,10 @@ import static android.view.View.VISIBLE;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
@@ -98,6 +101,10 @@ public class SampleEditFragment extends BaseEditFragment<FragmentSampleEditLayou
 	private List<Item> posNegEqList;
 	private List<Item> frequencyOfChangingFiltersList;
 	private List<Item> finalClassificationList;
+
+	private  List<Item> finalExamResultList;
+
+	private List<Item> injectionSiteList;
 
 	public static SampleEditFragment newInstance(Sample activityRootData) {
 		return newInstanceWithFieldCheckers(
@@ -489,6 +496,9 @@ public class SampleEditFragment extends BaseEditFragment<FragmentSampleEditLayou
 			case CHOLERA:
 				handleCholera();
 				break;
+			case AFP:
+				handleAFP();
+				break;
 			default:
 		}
 
@@ -498,6 +508,9 @@ public class SampleEditFragment extends BaseEditFragment<FragmentSampleEditLayou
 
 		finalClassificationList = DataUtils.getEnumItems(FinalClassification.class, true);
 		contentBinding.sampleAfpFinalClassification.initializeSpinner(finalClassificationList);
+
+		finalExamResultList = DataUtils.getEnumItems(ExamResult.class,true);
+		contentBinding.sampleResultExam.initializeSpinner(finalExamResultList);
 
 	}
 
@@ -632,6 +645,28 @@ public class SampleEditFragment extends BaseEditFragment<FragmentSampleEditLayou
 			int visibility = (field.getValue() == YesNo.YES ? View.VISIBLE : View.GONE);
 			getContentBinding().sampleSampleDateTime.setVisibility(visibility);
 		});
+
+	}
+
+	private void handleAFP() {
+		Set<InjectionSite> allowedInjections = EnumSet.of(
+				InjectionSite.LEFT_ARM,
+				InjectionSite.LEFT_LEG,
+				InjectionSite.RIGHT_ARM,
+				InjectionSite.RIGHT_LEG
+		);
+		injectionSiteList = DataUtils.toItems(
+				Arrays.stream(InjectionSite.values())
+						.filter(Objects::nonNull)
+						.filter(allowedInjections::contains)
+						.collect(Collectors.toList())
+		);
+		injectionSiteList = injectionSiteList.stream()
+				.filter(item -> item != null)
+				.filter(item -> item.getKey() != null && !item.getKey().trim().isEmpty())
+				.filter(item -> item.getValue() != null)
+				.collect(Collectors.toList());
+		getContentBinding().sampleResidualAnalysis.setEnumItems(injectionSiteList);
 
 	}
 
