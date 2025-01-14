7a2647b3e4047aaf340a1788bd4a92d675827207
douglasderrick <douglasder12@gmail.com>
2025-01-14 15:30:42 +0000

    Fix: Added Required annotation for Measles SampleMaterial

--- sormas-api/src/main/java/de/symeda/sormas/api/sample/SampleMaterial.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/sample/SampleMaterial.java
@@ -41,63 +41,151 @@ public enum SampleMaterial {
 			NEW_INFLUENZA,
 	})
 	ORO_NASO,
-
+	@Diseases(value = {
+	}, hide = true)
 	BLOOD,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	SERA,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	STOOL,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	NASAL_SWAB,
+	@Diseases(value = {
+	}, hide = true)
 	THROAT_SWAB,
 //	NP_SWAB,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	RECTAL_SWAB,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	CEREBROSPINAL_FLUID,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	CRUST,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	TISSUE,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
 	URINE,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	CORNEA_PM,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	SALIVA,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	URINE_PM,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	NUCHAL_SKIN_BIOPSY,
 	SPUTUM,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	ENDOTRACHEAL_ASPIRATE,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	BRONCHOALVEOLAR_LAVAGE,
 	@Diseases(value = {
-		Disease.CORONAVIRUS }, hide = true)
+		Disease.CORONAVIRUS, MEASLES }, hide = true)
 	BRAIN_TISSUE,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	ANTERIOR_NARES_SWAB,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	OP_ASPIRATE,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	NP_ASPIRATE,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	PLEURAL_FLUID,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	PLASMA,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	SERUM,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	POST_MORTEM_LIVER_SPECIMEN,
+	@Diseases(value = {
+	}, hide = true)
 	GINGIVAL_FLUID,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	PLASMA_SERUM,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	ASPIRATE,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	PUS,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	BIOPSY,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	URETHRAL,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	FOOD_WATER,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	BLOOD_ANTI_COAGULANT,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	CARDIAC,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	SWAB,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	VOMITUS,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	WATER,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	FOOD_ITEM,
+	@Diseases(value = {
+			MEASLES,
+	}, hide = true)
 	BLOOD_SERUM,
+	@Diseases(value = {
+	}, hide = true)
 	OTHER;
 
 	//getYellowFeverMateriealTypes
