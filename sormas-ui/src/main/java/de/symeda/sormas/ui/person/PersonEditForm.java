0e5cee77cd14e03029b640076efec869e214dae5
douglasderrick <douglasder12@gmail.com>
2025-01-22 17:39:59 +0000

    Fix: Fixed occupation error for measles

--- sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonEditForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonEditForm.java
@@ -834,7 +834,7 @@ public class PersonEditForm extends AbstractEditForm<PersonDto> {
 			generalCommentLabel.setVisible(false);
 			setVisible(false, PersonDto.ADDITIONAL_DETAILS);
 			setVisible(false, PersonDto.MOTHERS_MAIDEN_NAME);
-			setVisible(false, PersonDto.NICKNAME, OCCUPATION_HEADER);
+			setVisible(false, PersonDto.NICKNAME);
 			setVisible(true, PersonDto.GHANA_CARD, PersonDto.NATIONAL_HEALTH_ID);
 		}
 
