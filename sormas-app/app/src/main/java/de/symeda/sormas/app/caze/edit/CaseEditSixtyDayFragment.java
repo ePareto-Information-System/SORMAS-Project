d781142acb71b662537f012a5112f81e04a003fa
douglasderrick <douglasder12@gmail.com>
2025-01-27 10:01:23 +0000

    Feature: Added event listener to Paralysis Weakness Present Site to show Paralyzed Part Other

--- sormas-app/app/src/main/java/de/symeda/sormas/app/caze/edit/CaseEditSixtyDayFragment.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/caze/edit/CaseEditSixtyDayFragment.java
@@ -19,11 +19,14 @@ import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 
 import android.content.res.Resources;
+import android.view.View;
 
+import java.util.Collections;
 import java.util.List;
 
 import de.symeda.sormas.api.Disease;
 import de.symeda.sormas.api.FormType;
+import de.symeda.sormas.api.sample.PathogenTestType;
 import de.symeda.sormas.api.utils.InjectionSite;
 import de.symeda.sormas.api.utils.NormalWasted;
 import de.symeda.sormas.api.utils.PackagingType;
@@ -142,6 +145,23 @@ public class CaseEditSixtyDayFragment extends BaseEditFragment<FragmentCaseEditS
 		if (getContentBinding().sixtyDaySpecifySources.getValue() != null) {
 			getContentBinding().sixtyDaySpecifySources.setVisibility(VISIBLE);
 		}
+
+		applySampleTestsVisibility(getContentBinding().sixtyDayParalysisWeaknessPresentSite.getSelectedValues());
+
+		getContentBinding().sixtyDayParalysisWeaknessPresentSite.setOnValueChangeListener(selectedValues -> {
+			applySampleTestsVisibility(getContentBinding().sixtyDayParalysisWeaknessPresentSite.getSelectedValues());
+		});
+	}
+
+	private void applySampleTestsVisibility(List<String> selectedItems) {
+		if (selectedItems == null) {
+			selectedItems = Collections.emptyList();
+		}
+
+		boolean other = selectedItems.contains(ParalysisSite.OTHER.toString());
+		getContentBinding().sixtyDayParalyzedPartOther.setVisibility(other ? View.VISIBLE : View.GONE);
+
+
 	}
 
 	@Override
