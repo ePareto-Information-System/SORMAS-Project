405aa1cad41ad5eeefb1e37497df8bce1c30842b
anthony4m <amoahanthony41@gmail.com>
2025-01-31 16:47:17 +0000

    #183 feat: Add caption for EbsAlert reasonNoAlert (gh-release-1.87.0)
    Added caption "EbsAlert.reasonNoAlert" to Captions
    class for improved internationalization. UI update
    logic improved; it now dynamically updates the
    caption based on alert issued status. Layout
    formatting improved for better readability.

--- sormas-app/app/src/main/java/de/symeda/sormas/app/ebs/edit/SignalVerificationEditFragment.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/ebs/edit/SignalVerificationEditFragment.java
@@ -16,11 +16,14 @@
 package de.symeda.sormas.app.ebs.edit;
 
 import static de.symeda.sormas.api.utils.DataHelper.isNullOrEmpty;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.AlertDialog;
 import android.content.res.Resources;
 import android.widget.Toast;
-import java.util.Calendar;
-import java.util.Date;
+
 import de.symeda.sormas.api.ebs.SignalOutcome;
 import de.symeda.sormas.api.ebs.SignalVerificationDto;
 import de.symeda.sormas.api.i18n.I18nProperties;
@@ -34,16 +37,18 @@ import de.symeda.sormas.app.component.controls.ControlDateField;
 import de.symeda.sormas.app.component.controls.ControlTextEditField;
 import de.symeda.sormas.app.databinding.FragmentSignalVerificationEditLayoutBinding;
 
-public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSignalVerificationEditLayoutBinding, SignalVerification, SignalVerification> {
+public class SignalVerificationEditFragment
+	extends BaseEditFragment<FragmentSignalVerificationEditLayoutBinding, SignalVerification, SignalVerification> {
+
 	public static final String PERSON = "Person";
 	public static final String ANIMAL = "Animal";
-	public final String THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE = "The Verification Completed cannot be earlier than the Date of Report or Date of Occurrence or Decision Date.";
-	public final String THE_DATE_OF_OCCURRENCE_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE = "The Date of Occurrence cannot be earlier than the Date of Report or Date of Verification Completed Date.";
 	public static final String NUMBER_OF_DEATH_MORE_CASES = "The number of death cannot be more than cases";
 	public static final String TAG = SignalVerificationEditFragment.class.getSimpleName();
-
+	public final String THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE =
+		"The Verification Completed cannot be earlier than the Date of Report or Date of Occurrence or Decision Date.";
+	public final String THE_DATE_OF_OCCURRENCE_CANNOT_BE_OLDER_THAN_THE_DATE_OF_OCCURRENCE =
+		"The Date of Occurrence cannot be older than the Date of Report or Date of Verification Completed Date.";
 	private SignalVerification record;
-
 
 	public static SignalVerificationEditFragment newInstance(Ebs activityRootData) {
 		return newInstanceWithFieldCheckers(
@@ -53,6 +58,7 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 			null,
 			UiFieldAccessCheckers.forSensitiveData(activityRootData.isPseudonymized()));
 	}
+
 	public static SignalVerificationEditFragment newInstance(SignalVerification activityRootData) {
 		return newInstanceWithFieldCheckers(
 			SignalVerificationEditFragment.class,
@@ -96,32 +102,40 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 		setFieldVisibilitiesAndAccesses(SignalVerificationDto.class, contentBinding.mainContent);
 		contentBinding.signalVerificationVerificationCompleteDate.initializeDateField(getFragmentManager());
 		contentBinding.signalVerificationDateOfOccurrence.initializeDateField(getFragmentManager());
-		contentBinding.signalVerificationVerificationCompleteDate.addValueChangedListener(e->{
+		contentBinding.signalVerificationVerificationCompleteDate.addValueChangedListener(e -> {
 			validateSignalVerificationVerificationCompleteDate(contentBinding);
 		});
-		contentBinding.signalVerificationDateOfOccurrence.addValueChangedListener(e->{
+		contentBinding.signalVerificationDateOfOccurrence.addValueChangedListener(e -> {
 			signalVerificationDateOfOccurrence(contentBinding);
-			checkDeathCount(contentBinding, PERSON,true);
-			checkDeathCount(contentBinding, ANIMAL,true);
+			checkDeathCount(contentBinding, PERSON, true);
+			checkDeathCount(contentBinding, ANIMAL, true);
 		});
 
 		validateSignalVerificationVerificationCompleteDate(contentBinding);
 		signalVerificationDateOfOccurrence(contentBinding);
-		contentBinding.signalVerificationVerificationSent.addValueChangedListener(e->{
-			if (e.getValue() == YesNo.NO){
+		contentBinding.signalVerificationVerificationSent.addValueChangedListener(e -> {
+			if (e.getValue() == YesNo.NO) {
 				showAlert(R.string.review_verification_24hrs);
-			}else {
-				checkDeathCount(contentBinding, PERSON,true);
-				checkDeathCount(contentBinding, ANIMAL,true);
+			} else {
+				checkDeathCount(contentBinding, PERSON, true);
+				checkDeathCount(contentBinding, ANIMAL, true);
 			}
 		});
-		contentBinding.signalVerificationNumberOfDeathPerson.addValueChangedListener(e->{checkDeathCount(contentBinding, PERSON,false);});
-		contentBinding.signalVerificationNumberOfPersonCases.addValueChangedListener(e->{checkDeathCount(contentBinding,PERSON,false);});
-		contentBinding.signalVerificationNumberOfPersonAnimal.addValueChangedListener(e->{checkDeathCount(contentBinding, ANIMAL,false);});
-		contentBinding.signalVerificationNumberOfDeath.addValueChangedListener(e->{checkDeathCount(contentBinding,ANIMAL,false);});
-		contentBinding.signalVerificationDescription.addValueChangedListener(e->{
-			checkDeathCount(contentBinding,PERSON,true);
-			checkDeathCount(contentBinding,ANIMAL,true);
+		contentBinding.signalVerificationNumberOfDeathPerson.addValueChangedListener(e -> {
+			checkDeathCount(contentBinding, PERSON, false);
+		});
+		contentBinding.signalVerificationNumberOfPersonCases.addValueChangedListener(e -> {
+			checkDeathCount(contentBinding, PERSON, false);
+		});
+		contentBinding.signalVerificationNumberOfPersonAnimal.addValueChangedListener(e -> {
+			checkDeathCount(contentBinding, ANIMAL, false);
+		});
+		contentBinding.signalVerificationNumberOfDeath.addValueChangedListener(e -> {
+			checkDeathCount(contentBinding, ANIMAL, false);
+		});
+		contentBinding.signalVerificationDescription.addValueChangedListener(e -> {
+			checkDeathCount(contentBinding, PERSON, true);
+			checkDeathCount(contentBinding, ANIMAL, true);
 		});
 	}
 
@@ -131,20 +145,20 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 		ControlTextEditField fieldToValidate = null;
 
 		switch (entity) {
-			case PERSON:
-				numberOfCases = contentBinding.signalVerificationNumberOfPersonCases.getValue();
-				value = contentBinding.signalVerificationNumberOfDeathPerson.getValue();
-				fieldToValidate = contentBinding.signalVerificationNumberOfDeathPerson;
-				break;
-			case ANIMAL:
-				numberOfCases = contentBinding.signalVerificationNumberOfPersonAnimal.getValue();
-				value = contentBinding.signalVerificationNumberOfDeath.getValue();
-				fieldToValidate = contentBinding.signalVerificationNumberOfDeath;
-				break;
-			default:
-				return;
+		case PERSON:
+			numberOfCases = contentBinding.signalVerificationNumberOfPersonCases.getValue();
+			value = contentBinding.signalVerificationNumberOfDeathPerson.getValue();
+			fieldToValidate = contentBinding.signalVerificationNumberOfDeathPerson;
+			break;
+		case ANIMAL:
+			numberOfCases = contentBinding.signalVerificationNumberOfPersonAnimal.getValue();
+			value = contentBinding.signalVerificationNumberOfDeath.getValue();
+			fieldToValidate = contentBinding.signalVerificationNumberOfDeath;
+			break;
+		default:
+			return;
 		}
-		if (anotherField){
+		if (anotherField) {
 			CheckCaseNull(contentBinding, entity, value, numberOfCases);
 		}
 
@@ -156,9 +170,8 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 				if (deathCount > caseCount) {
 					ControlTextEditField finalFieldToValidate = fieldToValidate;
 					fieldToValidate.setValidationCallback(() -> {
-						finalFieldToValidate.enableErrorState(
-								I18nProperties.getValidationError(NUMBER_OF_DEATH_MORE_CASES, NUMBER_OF_DEATH_MORE_CASES)
-						);
+						finalFieldToValidate
+							.enableErrorState(I18nProperties.getValidationError(NUMBER_OF_DEATH_MORE_CASES, NUMBER_OF_DEATH_MORE_CASES));
 						return true;
 					});
 				} else {
@@ -171,18 +184,18 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 					return true;
 				});
 			}
-		}else {
+		} else {
 			fieldToValidate.setValidationCallback(() -> false);
 		}
 	}
 
 	private void CheckCaseNull(FragmentSignalVerificationEditLayoutBinding contentBinding, String entity, String value, String numberOfCases) {
-		if (!isNullOrEmpty(value) && isNullOrEmpty(numberOfCases) && entity.equals(PERSON)){
-			int valueToInt =Integer.parseInt(value);
+		if (!isNullOrEmpty(value) && isNullOrEmpty(numberOfCases) && entity.equals(PERSON)) {
+			int valueToInt = Integer.parseInt(value);
 			contentBinding.signalVerificationNumberOfPersonCases.setValue(String.valueOf(valueToInt));
 		}
-		if (!isNullOrEmpty(value) && isNullOrEmpty(numberOfCases) && entity.equals(ANIMAL)){
-			int valueToInt =Integer.parseInt(value);
+		if (!isNullOrEmpty(value) && isNullOrEmpty(numberOfCases) && entity.equals(ANIMAL)) {
+			int valueToInt = Integer.parseInt(value);
 			contentBinding.signalVerificationNumberOfPersonAnimal.setValue(String.valueOf(valueToInt));
 		}
 	}
@@ -203,26 +216,30 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 
 		Date dateOfReportDate = clearTime(dateOfReport);
 		Date dateOfDecisionDate = clearTime(dateOfDecision);
-		if (DateverificationCompleteDate != null){
+		if (DateverificationCompleteDate != null) {
 			DateverificationCompleteDate = clearTime(DateverificationCompleteDate);
 			if (DateverificationCompleteDate.before(dateOfReportDate) || DateverificationCompleteDate.before(dateOfDecisionDate)) {
-				if (!dateOfReportDate.toString().equals(DateverificationCompleteDate.toString()) || !dateOfDecision.toString().equals(DateverificationCompleteDate.toString())) {
+				if (!dateOfReportDate.toString().equals(DateverificationCompleteDate.toString())
+					|| !dateOfDecision.toString().equals(DateverificationCompleteDate.toString())) {
 					showError(THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE);
 					verificationCompleteDate.setValidationCallback(() -> {
-						verificationCompleteDate.enableErrorState(I18nProperties.getValidationError(THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE, THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE));
+						verificationCompleteDate.enableErrorState(
+							I18nProperties.getValidationError(
+								THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE,
+								THE_DATE_OF_DECISION_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE));
 						return true;
 					});
-				}else {
+				} else {
 					verificationCompleteDate.setValidationCallback(() -> false);
 				}
+			} else {
+				verificationCompleteDate.setValidationCallback(() -> false);
 			}
-			else {verificationCompleteDate.setValidationCallback(() -> false);}
 		}
 	}
 
 	public void signalVerificationDateOfOccurrence(FragmentSignalVerificationEditLayoutBinding contentBinding) {
 		Date dateOfReport = EbsEditActivity.getParentEbs().getReportDateTime();
-		Date dateOfDecision = EbsEditActivity.getParentEbs().getTriaging().getDecisionDate();
 		ControlDateField verificationCompleteDate = contentBinding.signalVerificationVerificationCompleteDate;
 		ControlDateField signalVerificationDateOfOccurrence = contentBinding.signalVerificationDateOfOccurrence;
 		Date DatesignalVerificationDateOfOccurrenceDate = signalVerificationDateOfOccurrence.getValue();
@@ -234,24 +251,27 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 		}
 
 		Date dateOfReportDate = clearTime(dateOfReport);
-		Date dateOfDecisionDate = clearTime(dateOfDecision);
-		Date DateverificationCompleteDate = clearTime(verificationCompleteDate.getValue());
-		if (DatesignalVerificationDateOfOccurrenceDate != null){
+		if (DatesignalVerificationDateOfOccurrenceDate != null) {
 			DatesignalVerificationDateOfOccurrenceDate = clearTime(DatesignalVerificationDateOfOccurrenceDate);
-			if (DatesignalVerificationDateOfOccurrenceDate.before(dateOfReportDate) || DatesignalVerificationDateOfOccurrenceDate.before(dateOfDecisionDate) || DatesignalVerificationDateOfOccurrenceDate.before(DateverificationCompleteDate)) {
-				if (!dateOfReportDate.toString().equals(DatesignalVerificationDateOfOccurrenceDate.toString()) || !dateOfDecision.toString().equals(DatesignalVerificationDateOfOccurrenceDate.toString())) {
-					showError(THE_DATE_OF_OCCURRENCE_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE);
+			if (DatesignalVerificationDateOfOccurrenceDate.after(dateOfReportDate)) {
+				if (!dateOfReportDate.toString().equals(DatesignalVerificationDateOfOccurrenceDate.toString())) {
+					showError(THE_DATE_OF_OCCURRENCE_CANNOT_BE_OLDER_THAN_THE_DATE_OF_OCCURRENCE);
 					signalVerificationDateOfOccurrence.setValidationCallback(() -> {
-						signalVerificationDateOfOccurrence.enableErrorState(I18nProperties.getValidationError(THE_DATE_OF_OCCURRENCE_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE, THE_DATE_OF_OCCURRENCE_CANNOT_BE_EARLIER_THAN_THE_DATE_OF_OCCURRENCE));
+						signalVerificationDateOfOccurrence.enableErrorState(
+							I18nProperties.getValidationError(
+								THE_DATE_OF_OCCURRENCE_CANNOT_BE_OLDER_THAN_THE_DATE_OF_OCCURRENCE,
+								THE_DATE_OF_OCCURRENCE_CANNOT_BE_OLDER_THAN_THE_DATE_OF_OCCURRENCE));
 						return true;
 					});
-				}else {
+				} else {
 					signalVerificationDateOfOccurrence.setValidationCallback(() -> false);
 				}
+			} else {
+				signalVerificationDateOfOccurrence.setValidationCallback(() -> false);
 			}
-			else {signalVerificationDateOfOccurrence.setValidationCallback(() -> false);}
 		}
 	}
+
 	private Date clearTime(Date date) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
@@ -261,13 +281,13 @@ public class SignalVerificationEditFragment extends BaseEditFragment<FragmentSig
 		calendar.set(Calendar.MILLISECOND, 0);
 		return calendar.getTime();
 	}
+
 	private void showError(String message) {
 		Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
 	}
-	public void showAlert(int message){
+
+	public void showAlert(int message) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-		builder.setTitle(R.string.indicator_warning)
-				.setMessage(message)
-				.show();
+		builder.setTitle(R.string.indicator_warning).setMessage(message).show();
 	}
 }
