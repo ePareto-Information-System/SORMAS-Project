46dae5c6186340efdd93be6ae01380778aa437ba
Flava177 <flavaflav177@gmail.com>
2025-03-14 15:00:52 +0000

    Resolved issue with changing source case from contact

--- sormas-backend/src/main/java/de/symeda/sormas/backend/caze/transformers/CaseSelectionDtoResultTransformer.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/caze/transformers/CaseSelectionDtoResultTransformer.java
@@ -18,12 +18,12 @@ public class CaseSelectionDtoResultTransformer implements ResultTransformer {
 
 	@Override
 	public Object transformTuple(Object[] objects, String[] strings) {
-		Integer age = objects[6] != null ? (int) objects[6] : null;
-		ApproximateAgeType approximateAgeType = (ApproximateAgeType) objects[7];
-		Integer birthdateDD = objects[8] != null ? (int) objects[8] : null;
-		Integer birthdateMM = objects[9] != null ? (int) objects[9] : null;
-		Integer birthdateYYYY = objects[10] != null ? (int) objects[10] : null;
-		String healthFacilityName = FacilityHelper.buildFacilityString((String) objects[12], (String) objects[13], (String) objects[14]);
+		Integer age = objects[7] != null ? (int) objects[7] : null;
+		ApproximateAgeType approximateAgeType = (ApproximateAgeType) objects[8];
+		Integer birthdateDD = objects[9] != null ? (int) objects[9] : null;
+		Integer birthdateMM = objects[10] != null ? (int) objects[10] : null;
+		Integer birthdateYYYY = objects[11] != null ? (int) objects[11] : null;
+		String healthFacilityName = FacilityHelper.buildFacilityString((String) objects[13], (String) objects[14], (String) objects[15]);
 		return new CaseSelectionDto(
 			(String) objects[0],
 			(String) objects[1],
@@ -35,11 +35,11 @@ public class CaseSelectionDtoResultTransformer implements ResultTransformer {
 			new AgeAndBirthDateDto(age, approximateAgeType, birthdateDD, birthdateMM, birthdateYYYY),
 			(String) objects[11],
 			healthFacilityName,
-			(Date) objects[15],
-			(Sex) objects[16],
-			(CaseClassification) objects[17],
-			(CaseOutcome) objects[18],
-			(Boolean) objects[19]);
+			(Date) objects[16],
+			(Sex) objects[17],
+			(CaseClassification) objects[18],
+			(CaseOutcome) objects[19],
+			(Boolean) objects[20]);
 	}
 
 	@Override
