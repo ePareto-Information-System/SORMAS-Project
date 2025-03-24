a6ade9e6717570d29606cd69c5fc4883131c8520
douglasderrick <douglasder12@gmail.com>
2025-03-24 14:51:04 +0000

    Fix: Comment out SVG download option in AbstractEpiCurveBuilder

--- sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/diagram/AbstractEpiCurveBuilder.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/diagram/AbstractEpiCurveBuilder.java
@@ -93,7 +93,7 @@ public abstract class AbstractEpiCurveBuilder<C extends BaseDashboardCriteria<C>
 				"                    'downloadPNG',\n" +
 				"                    'downloadJPEG',\n" +
 				"                    'downloadPDF',\n" +
-				"                    'downloadSVG',\n" +
+//				"                    'downloadSVG',\n" +
 				"                    'downloadCSV',\n" +
 				"                    'downloadXLS'\n" +
 				"                ]\n" +
