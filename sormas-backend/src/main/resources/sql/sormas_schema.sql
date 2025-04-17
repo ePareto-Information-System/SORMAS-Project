58a0b595f322ef55e6c7367c1f331ead945c9b79
Flava177 <flavaflav177@gmail.com>
2025-04-17 10:57:43 +0000

    Updated the comment name in sormas schema file for version 648

--- sormas-backend/src/main/resources/sql/sormas_schema.sql
+++ sormas-backend/src/main/resources/sql/sormas_schema.sql
@@ -14738,4 +14738,4 @@ ALTER TABLE contacts_visits REPLICA IDENTITY FULL;
 INSERT INTO schema_version(version_number, comment) VALUES (647, 'Resolved replica saving error with contacts_visits table');
 
 UPDATE samples SET samplematerial = 'NASOPHARYNGEAL_SWAB' WHERE samplematerial = 'NP_SWAB';
-INSERT INTO schema_version(version_number, comment) VALUES (648, 'Resolved replica saving error with contacts_visits table');
+INSERT INTO schema_version(version_number, comment) VALUES (648, 'updated np_swab to NASOPHARYNGEAL_SWAB');
